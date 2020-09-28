be144088f516acf43249391fdb377096dc5d27cb
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-09-28 10:47:26 +0200

    guardato dataset degli incidenti, ho una mappa iniziale con posizionamento dei sinistri. Ho usato geopandas e contextily

--- geo_incidenti.py
+++ geo_incidenti.py
@@ -1,7 +1,15 @@
 
+import matplotlib.pyplot as plt
 import geopandas as gpd
+import contextily as cx
 
 path = "/Users/gabrielepadovani/Desktop/Università/Data/tesi/Tesi/dataset/incidenti/inc_strad_milano_2016.geojson"
 
-file = gpd.read_file(path)
-print(file)
+data = gpd.read_file(path)
+#print(type(data))
+#world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+#print(gpd.datasets.available)
+
+ax = data.plot(color="red", figsize=(13,10), alpha=0.05)
+cx.add_basemap(ax, crs=data.crs.to_string())
+plt.show()
