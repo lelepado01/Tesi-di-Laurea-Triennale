8d30324929c909bea2bbe5f8de101bf9af693772
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-09-30 13:20:36 +0200

    aggiunte mappe per autovelox+incidenti

--- src/autovelox/autovelox+incidenti.py
+++ src/autovelox/autovelox+incidenti.py
@@ -14,10 +14,11 @@ path_autovelox = "tesi/Tesi/dataset/autovelox/autovelox_milano.geojson"
 dati_incidenti = gp.read_file(path_incidenti).to_crs(epsg=3857)
 dati_autovelox = gp.read_file(path_autovelox).to_crs(epsg=3857)
 
-#layer_incidenti = dati_incidenti.plot(figsize=(11,9), color="blue", alpha=0.1)
-#layer_autovelox = dati_autovelox.plot(ax=layer_incidenti, color="red")
-#cx.add_basemap(ax=layer_autovelox)
-#plt.show() 
+# MAPPA 1
+layer_incidenti = dati_incidenti.plot(figsize=(11,9), color="blue", alpha=0.1)
+layer_autovelox = dati_autovelox.plot(ax=layer_incidenti, color="red")
+cx.add_basemap(ax=layer_autovelox)
+plt.show() 
 
 # dovrei togliere i punti in alto (autovelox) che non rientrano nei dati incidenti
 
@@ -34,6 +35,7 @@ autovelox_ridotti = dati_autovelox[utils.remove_points_out_of_range(
 
 #print(len(autovelox_ridotti))
 
+# MAPPA 2
 layer_incidenti = dati_incidenti.plot(figsize=(11,9), color="blue", alpha=0.1)
 layer_autovelox = autovelox_ridotti.plot(ax=layer_incidenti, color="red")
 cx.add_basemap(ax=layer_autovelox)
