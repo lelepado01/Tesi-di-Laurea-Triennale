68bec83e90c4e4e39f2bf791cc7a0eb743f36630
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-02 11:10:23 +0200

    refactoring

--- src/html_parser.py
+++ src/html_parser.py
@@ -15,10 +15,8 @@ class MyHTMLParser(HTMLParser):
 
     def handle_data(self, data):
         if self.start_line == "": 
-            #print(data)
             self.start_line = str(data) 
             filename = self.start_line.split(" ")[4]
-            #print(self.path + filename)
             self.file = open(self.path + filename + ".csv", "w")
         else: 
             self.file.write(data)
@@ -26,12 +24,11 @@ class MyHTMLParser(HTMLParser):
 dir_path = 'dataset/incidenti/Classificazioni/'
 
 res = os.listdir(dir_path)
-#print(res)
+
 for f in res: 
     if not (os.path.isfile(dir_path + str(f)) and str(f).endswith(".html")): 
         res.remove(f)
 
-#print(res)
 
 for file_path in res: 
     file = open(dir_path + file_path).read()
