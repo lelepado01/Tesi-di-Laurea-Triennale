16f065539445635a608aa70bf8dd4425d269f149
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-05 13:05:47 +0200

    condizioni meteo utilizzando nuovo dataset di ilmeteo

--- src/incidenti/incidenti_senza_coords/condizioni_meteo/condizioni_meteo.py
+++ src/incidenti/incidenti_senza_coords/condizioni_meteo/condizioni_meteo.py
@@ -19,8 +19,53 @@ meteo = data['condizioni_meteorologiche']
 
 path_meteo = "dataset/meteo/ilmeteo2010/Milano-2010-Luglio.csv"
 
-dati_meteo = pd.read_csv(path_meteo, sep=';')
-dati_meteo
-print(dati_meteo['FENOMENI'].value_counts())
+dati_meteo = pd.read_csv(path_meteo, sep=';').fillna("sereno") 
+
+#print(dati_meteo['FENOMENI'].value_counts())
 
 # creo funzione per sapere la percentuale dei giorni in cui sono avvenuti fenomeni particolari
+
+paths = [
+    "dataset/meteo/ilmeteo2010/Milano-2010-Gennaio.csv",
+    "dataset/meteo/ilmeteo2010/Milano-2010-Febbraio.csv",
+    "dataset/meteo/ilmeteo2010/Milano-2010-Marzo.csv",
+    "dataset/meteo/ilmeteo2010/Milano-2010-Aprile.csv",
+    "dataset/meteo/ilmeteo2010/Milano-2010-Maggio.csv",
+    "dataset/meteo/ilmeteo2010/Milano-2010-Giugno.csv",
+    "dataset/meteo/ilmeteo2010/Milano-2010-Luglio.csv",
+    "dataset/meteo/ilmeteo2010/Milano-2010-Agosto.csv",
+    "dataset/meteo/ilmeteo2010/Milano-2010-Settembre.csv",
+    "dataset/meteo/ilmeteo2010/Milano-2010-Ottobre.csv",
+    "dataset/meteo/ilmeteo2010/Milano-2010-Novembre.csv",
+    "dataset/meteo/ilmeteo2010/Milano-2010-Dicembre.csv",
+]
+
+def add_values_to_dict(dictionary : dict, df : pd.Series): 
+    for index, val in zip(df.index, df.values): 
+        if index in dictionary: 
+            dictionary[index] = dictionary[index] + val
+        else: 
+            dictionary[index] = val
+
+    return dictionary
+
+percentuali_annuali = {}
+for path in paths: 
+    dati_meteo = pd.read_csv(path, sep=';').fillna("sereno") 
+    percentuali_annuali = add_values_to_dict(percentuali_annuali, dati_meteo['FENOMENI'].value_counts()) 
+
+#print(percentuali_annuali)
+
+# posso controllare se la funzione è giusta sommando le istanze
+#print(sum(list(percentuali_annuali.values())))
+# restituisce 365 giorni
+
+# Ora normalizzo
+for k, v in zip(percentuali_annuali.keys(), percentuali_annuali.values()): 
+    percentuali_annuali[k] = v / 365
+
+print(percentuali_annuali)
+
+# è stato sereno il 40% delle giornate durante il 2010
+# IMP: il dataset di ilmeteo non tiene conto di grandine, mentre il dataset istat si
+
