bc9cd8a489e18eb37f308f6445f717cc5a41c18c
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-05 17:36:29 +0200

    grafo delle condizioni meteo pesate

--- src/incidenti/incidenti_senza_coords/condizioni_meteo/condizioni_meteo.py
+++ src/incidenti/incidenti_senza_coords/condizioni_meteo/condizioni_meteo.py
@@ -19,7 +19,8 @@ meteo = data['condizioni_meteorologiche']
 
 path_meteo = "dataset/meteo/ilmeteo2010/Milano-2010-Luglio.csv"
 
-dati_meteo = pd.read_csv(path_meteo, sep=';').fillna("sereno") 
+# Sostituisco tutti i valori "" con 'sereno' 
+dati_meteo_ilmeteo = pd.read_csv(path_meteo, sep=';').fillna("sereno")['FENOMENI']
 
 #print(dati_meteo['FENOMENI'].value_counts())
 
@@ -41,10 +42,11 @@ paths = [
 ]
 
 def add_values_to_dict(dictionary : dict, df : pd.Series): 
+    indici = ['sereno', 'pioggia', 'temporale', 'neve', 'nebbia']
     for index, val in zip(df.index, df.values): 
         if index in dictionary: 
             dictionary[index] = dictionary[index] + val
-        else: 
+        elif index in indici: 
             dictionary[index] = val
 
     return dictionary
@@ -64,8 +66,33 @@ for path in paths:
 for k, v in zip(percentuali_annuali.keys(), percentuali_annuali.values()): 
     percentuali_annuali[k] = v / 365
 
-print(percentuali_annuali)
+#print(percentuali_annuali)
 
 # è stato sereno il 40% delle giornate durante il 2010
 # IMP: il dataset di ilmeteo non tiene conto di grandine, mentre il dataset istat si
 
+# Se moltiplico percentuali_annuali per i dati del meteo: 
+# Mi interessano sereno, pioggia, neve e nebbia
+
+number_to_label = {
+    1 : "sereno", 
+    2 : "nebbia", 
+    3 : "pioggia", 
+    5 : "neve"
+}
+
+#print(percentuali_annuali)
+#print(meteo.value_counts())
+
+incidenti_pesati = {}
+for index, val in zip(meteo.value_counts().index, meteo.value_counts()):
+    if index in number_to_label.keys(): 
+        incidenti_pesati[number_to_label.get(index)] = val * (1- percentuali_annuali[number_to_label.get(index)])
+
+#print(incidenti_pesati)
+
+pd.Series(incidenti_pesati).plot.bar()
+plt.show()
+
+# Il grafo non mi dice molto... potrei calcolare il coeff. di pearson delle due variabili
+# ma devo fare in modo che sia possibile calcolare la covarianza tra i due set 
