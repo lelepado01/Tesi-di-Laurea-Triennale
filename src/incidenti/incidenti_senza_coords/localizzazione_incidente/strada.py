e3c86f9a1958c2880fe28d5fb94f1c449d4614a0
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-06 15:10:04 +0200

    guardato altri campi del dataset incidenti

--- src/incidenti/incidenti_senza_coords/localizzazione_incidente/strada.py
+++ src/incidenti/incidenti_senza_coords/localizzazione_incidente/strada.py
@@ -20,8 +20,25 @@ tipo_strada = data['tipo_di_strada'].value_counts().sort_index()
 pavimentazione = data['pavimentazione']
 #print(pavimentazione.unique())
 
-pavimentazione.value_counts().sort_index().plot.bar()
-plt.show()
+#pavimentazione.value_counts().sort_index().plot.bar()
+#plt.show()
 
 # Per volume le strade pavimentate hanno più incidenti...
 # Per caso influisce fatto che alcune strade hanno pavimentazione a blocchi (PAVE')?
+
+segnaletica = data['segnaletica']
+#print(segnaletica.unique())
+
+#segnaletica.value_counts().sort_index().plot.bar()
+#plt.show()
+
+# Qualcosa non va perchè col.4 dovrebbe essere col.3 + col.2
+
+labels_intersezione = pd.read_csv("dataset/incidenti/Classificazioni/intersezione_o_non_interse3.csv")
+print(labels_intersezione['Descrizione'])
+
+intersezione = data['intersezione_o_non_interse1']
+#print(intersezione.unique())
+
+intersezione.value_counts().sort_index().plot.bar()
+plt.show()
