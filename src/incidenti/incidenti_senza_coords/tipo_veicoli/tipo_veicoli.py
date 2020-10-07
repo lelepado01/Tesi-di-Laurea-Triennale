198427a94639520ac0b7464243dbaf421e2d5231
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-07 14:55:05 +0200

    continuato file su tipo di veicoli

---
+++ src/incidenti/incidenti_senza_coords/tipo_veicoli/tipo_veicoli.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+path = "dataset/incidenti/incidenti_2010.txt"
+
+data = pd.read_csv(path, sep="\t")
+
+tipo_veicoli = data['tipo_veicolo_a'].value_counts().sort_index()
+#print(tipo_veicoli)
+
+tipo_veicoli[tipo_veicoli > 100].plot.bar()
+plt.show()
+
+# I principali veicoli coinvolti sono: 
+#   - auto private
+#   - moto private
+#   - autocarri
+ 
+# TODO: altri dati
