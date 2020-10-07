4049a2228bb31566d2c470afcc7beb4dd2c25f13
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-07 16:44:24 +0200

    aggiunte descrizioni alle funzioni del file label_utils

--- src/label_utils.py
+++ src/label_utils.py
@@ -1,6 +1,9 @@
 
 import pandas as pd
 
+# Converte il dataframe passato come argomento, con campi <Modalità, Descrizione>
+# in un dizionario
+# uso la funzione per rimpiazzare gli indici numerici di altri dataframe  
 def dataframe_to_dict(df : pd.DataFrame) -> dict: 
     campi = ['Modalita\'', 'Descrizione']
     dictionary : dict = {} 
@@ -9,6 +12,9 @@ def dataframe_to_dict(df : pd.DataFrame) -> dict:
     
     return dictionary
 
-def join_labels(dataframe : pd.DataFrame, path_to_labels) -> pd.DataFrame: 
+# dataframe : dati in formato numerico da rimpiazzare
+# path_to_labels : path al file di labels
+# La funzione restituisce un dataframe modificato
+def join_labels(dataframe : pd.DataFrame, path_to_labels : str) -> pd.DataFrame: 
     labels = pd.read_csv(path_to_labels, sep=',')
     return dataframe.replace(dataframe_to_dict(labels))
