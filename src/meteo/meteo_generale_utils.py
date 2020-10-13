d469ddff7ba6f033bb872c52e584e98260361aad
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-13 11:43:41 +0200

    documentazione funzioni

--- src/meteo/meteo_generale_utils.py
+++ src/meteo/meteo_generale_utils.py
@@ -1,15 +1,18 @@
 
-# TODO: documentazione
+# Le funzioni restituiscono un dataframe in cui vengono filtrati i campi che non corrispondono
+# all'anno / mese / giorno
+# Il Dataset passato deve contenere un campo 'Date_Time' nel formato: 
+# mm/dd/yyyy hh:hh 
 
 import pandas as pd
 
-def get_weather_by_year(meteo : pd.DataFrame, year : int) -> pd.Series: 
+def get_weather_by_year(meteo : pd.DataFrame, year : int) -> pd.DataFrame: 
     return meteo[meteo['Date_Time'].map(lambda x : int(x.split(' ')[0].split('/')[2]) == year)]
 
-def get_weather_by_month(meteo : pd.DataFrame, month : int) -> pd.Series: 
+def get_weather_by_month(meteo : pd.DataFrame, month : int) -> pd.DataFrame: 
     return meteo[meteo['Date_Time'].map(lambda x : int(x.split(' ')[0].split('/')[0]) == month)]
 
-def get_weather_by_day(meteo : pd.DataFrame, day : int) -> pd.Series: 
+def get_weather_by_day(meteo : pd.DataFrame, day : int) -> pd.DataFrame: 
     return meteo[meteo['Date_Time'].map(lambda x : int(x.split(' ')[0].split('/')[1]) == day)]
 
 def get_hour(meteo : pd.Series) -> pd.Series: 
