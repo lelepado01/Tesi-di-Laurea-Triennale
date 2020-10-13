79202fcf220655c6352f1b892c7c7af2075691f8
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-13 10:33:24 +0200

    meteo generale: calcolata temperatura media per ogni anno

--- src/meteo/meteo_generale.py
+++ src/meteo/meteo_generale.py
@@ -12,7 +12,7 @@ import meteo_generale_utils as mgu
 meteo = pd.read_csv("dataset/meteo/meteo_milano_generale_2008_2019.csv", sep=",")
 
 meteo_2010 = mgu.get_weather_by_year(meteo, 2010)
-#print(meteo_2010)
+print(meteo_2010.columns)
 
 #meteo_2010['Temperature'].value_counts().sort_index().plot()
 #plt.show()
@@ -29,10 +29,27 @@ meteo_2010 = mgu.get_weather_by_year(meteo, 2010)
 #plt.plot(ora_temp['Temperature'])
 #plt.show()
 
-for year in range(2008, 2018): 
-    ora_temp = mgu.get_weather_by_year(meteo, year)
-    ora_temp = mgu.get_weather_by_month(ora_temp, 8)
-    ora_temp = mgu.get_weather_by_day(ora_temp, 1)[['Date_Time', 'Temperature']]
-    plt.plot(mgu.get_hour(ora_temp), ora_temp['Temperature'])
+#for year in range(2008, 2018): 
+#    ora_temp = mgu.get_weather_by_year(meteo, year)
+#    ora_temp = mgu.get_weather_by_month(ora_temp, 8)
+#    ora_temp = mgu.get_weather_by_day(ora_temp, 1)[['Date_Time', 'Temperature']]
+#    plt.plot(mgu.get_hour(ora_temp), ora_temp['Temperature'])
+#
+#plt.show()
 
+# Temperatura media in italia?
+media = pd.Series()
+for year in range(2008, 2019): 
+    meteo_anno = mgu.get_weather_by_year(meteo, year)
+    media_temp = meteo_anno['Humidity'].mean()
+    media = media.append(pd.Series(media_temp, index=[year]))
+
+#print(media)
+
+media.plot.bar()
 plt.show()
+
+# La temperatura sembra rimanere stabile, anche se 2016, 2017, 2018 sono in crescita
+# Anche la varianza della temperatura non sembra cambiare
+# La velocità del vento media aumenta (?) è un errore?
+# La varianza rimane costante
