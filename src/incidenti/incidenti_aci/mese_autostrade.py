29b731d1dddc1aec3ec8515fe6ce2a7247da755d
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-15 16:36:01 +0200

    aggiunti grafi su nuovi dati, in particolare incidenti sulla base dei mesi

--- src/incidenti/incidenti_aci/mese_autostrade.py
+++ src/incidenti/incidenti_aci/mese_autostrade.py
@@ -15,14 +15,82 @@ data = pd.read_csv("dataset/incidenti/aci/autostrade/mesi_2018.csv")
 lombardia = data[data['REGIONE'] == 'Lombardia'][['PROVINCIA', 'TOTALE']]
 #print(lombardia)
 
-res = {}
-for prov in lombardia['PROVINCIA'].unique(): 
-    res[prov] = sum(lombardia[lombardia['PROVINCIA'] == prov]['TOTALE'])
+def get_sum_fields(data : pd.DataFrame, select_field : str, field_to_sum : str) -> pd.Series: 
+    res = {}
+    for field in data[select_field].unique(): 
+        res[field] = sum(data[data[select_field] == field][field_to_sum])
 
-lombardia = pd.Series(res)
-lombardia.plot.bar()
-plt.tight_layout()
-plt.show()
+    return pd.Series(res)
+
+def sum_columns(data : pd.DataFrame, normalize = False) -> pd.Series: 
+    res = {}
+    for col in data.columns: 
+        res[col] = sum(data[col])
+
+    res = pd.Series(res).transpose()
+
+    if normalize: 
+        res = res / sum(res)
+
+    return res
+
+#res = {}
+#for prov in lombardia['PROVINCIA'].unique(): 
+#    res[prov] = sum(lombardia[lombardia['PROVINCIA'] == prov]['TOTALE'])
+
+#lombardia = get_sum_fields(data, 'REGIONE', 'TOTALE')
+#lombardia.plot.bar()
+#plt.tight_layout()
+#plt.show()
 
 # Per come sono organizzati, i dati sono un po' scomodi
 
+# Voglio vedere la differenza per mese tra due autostrade diverse
+
+mesi = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'] 
+
+rimini = data[data['PROVINCIA'] == 'Rimini'][mesi]
+milano = data[data['PROVINCIA'] == 'Milano'][mesi]
+aosta = data[data['PROVINCIA'] == 'Aosta'][mesi]
+roma = data[data['PROVINCIA'] == 'Roma'][mesi]
+
+rimini = sum_columns(rimini)
+milano = sum_columns(milano)
+aosta = sum_columns(aosta)
+roma = sum_columns(roma)
+
+# Normalizzo
+#rimini = rimini / sum(rimini)
+#milano = milano / sum(milano)
+#aosta = aosta / sum(aosta)
+#roma =  roma / sum(roma)
+
+#pd.DataFrame([milano, rimini], index=['Milano', 'Rimini']).transpose().plot.bar()
+#pd.DataFrame([milano, roma], index=['Milano', 'Roma']).transpose().plot.bar()
+#plt.show()
+
+#print(sum_columns(rimini))
+
+# Quali sono le autostrade con più incidenti?
+
+#autostrade = get_sum_fields(data, 'NOME STRADA', 'TOTALE').sort_values(ascending=True)
+#autostrade[autostrade > 400].plot.barh()
+#plt.tight_layout()
+#plt.xlabel("Autostrade con più incidenti totali")
+#plt.show()
+
+#print(autostrade)
+
+# E in quali mesi avvengono questi incidenti?
+
+adriatica = data[data['CODICE'] == 'SS01601'][mesi]
+a1 = data[data['CODICE'] == 'AA00101'][mesi]
+aurelia = data[data['CODICE'] == 'SS00101'][mesi]
+
+adriatica = sum_columns(adriatica, normalize=True)
+a1 = sum_columns(a1, normalize=True)
+aurelia =  sum_columns(aurelia, normalize=True)
+
+pd.DataFrame([a1, adriatica, aurelia], ['A1', 'Adriatica', 'Aurelia']).transpose().plot()
+plt.xlabel("Differenza tra incidenti mensili")
+plt.show()
