1229b642cc12a1f240fd6a5c6e5d4b8e47ce1724
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-16 09:55:19 +0200

    realizzato alcuni grafi  e scritto  parte di tesi

--- src/incidenti/incidenti_senza_coords/tipo_veicoli/tipo_veicoli.py
+++ src/incidenti/incidenti_senza_coords/tipo_veicoli/tipo_veicoli.py
@@ -22,8 +22,8 @@ tipo_veicoli = label_utils.join_labels(tipo_veicoli, "dataset/incidenti/Classifi
 #   - autocarri
 
 # Cambia se seleziono strade differenti?
-autostrade_veicoli = data[data['localizzazione_incidente'] == 7]['tipo_veicolo_a']
-autostrade_veicoli = label_utils.join_labels(autostrade_veicoli, "dataset/incidenti/Classificazioni/tipo_veicoli__b_.csv").value_counts(normalize=True).sort_index()
+#autostrade_veicoli = data[data['localizzazione_incidente'] == 7]['tipo_veicolo_a']
+#autostrade_veicoli = label_utils.join_labels(autostrade_veicoli, "dataset/incidenti/Classificazioni/tipo_veicoli__b_.csv").value_counts(normalize=True).sort_index()
 
 #uniti = pd.DataFrame([
 #    tipo_veicoli, autostrade_veicoli
@@ -32,22 +32,68 @@ autostrade_veicoli = label_utils.join_labels(autostrade_veicoli, "dataset/incide
 #autostrade_veicoli.plot.barh()
 #plt.show()
 
-strade_urbane = data[(data['localizzazione_incidente'] == 1) | (data['localizzazione_incidente'] == 2) | (data['localizzazione_incidente'] == 3)]['tipo_veicolo_a']
-strade_urbane = label_utils.join_labels(strade_urbane, "dataset/incidenti/Classificazioni/tipo_veicoli__b_.csv").value_counts(normalize=True).sort_index()
+#strade_urbane = data[(data['localizzazione_incidente'] == 1) | (data['localizzazione_incidente'] == 2) | (data['localizzazione_incidente'] == 3)]['tipo_veicolo_a']
+#strade_urbane = label_utils.join_labels(strade_urbane, "dataset/incidenti/Classificazioni/tipo_veicoli__b_.csv").value_counts(normalize=True).sort_index()
 
 #strade_urbane.plot.barh()
 #plt.show()
 
-strade_extraurbane = data[(data['localizzazione_incidente'] == 4) | (data['localizzazione_incidente'] == 5) | (data['localizzazione_incidente'] == 6)]['tipo_veicolo_a']
-strade_extraurbane = label_utils.join_labels(strade_extraurbane, "dataset/incidenti/Classificazioni/tipo_veicoli__b_.csv").value_counts(normalize=True).sort_index()
+#strade_extraurbane = data[(data['localizzazione_incidente'] == 4) | (data['localizzazione_incidente'] == 5) | (data['localizzazione_incidente'] == 6)]['tipo_veicolo_a']
+#strade_extraurbane = label_utils.join_labels(strade_extraurbane, "dataset/incidenti/Classificazioni/tipo_veicoli__b_.csv").value_counts(normalize=True).sort_index()
 
-uniti = pd.DataFrame([
-    autostrade_veicoli, strade_urbane, strade_extraurbane
-], index=['Autostrade', 'Strade urbane', 'Strade Extra-Urbane']).transpose()
+#uniti = pd.DataFrame([
+#    autostrade_veicoli, strade_urbane, strade_extraurbane
+#], index=['Autostrade', 'Strade urbane', 'Strade Extra-Urbane']).transpose()
 
-uniti.dropna().plot.bar()
-plt.tight_layout()
-plt.show()
+#uniti[uniti > 0.001].dropna().plot.bar()
+#plt.tight_layout()
+#plt.show()
 
 # Nelle strade urbane ci sono molti più incidenti con velocipedi e ciclomotori, 
 # mentre nelle autostrade con autocarri e automobili
+
+
+# Il numero di uomini / donne alla guida cambia al cambiare del tipo di strada?
+
+#strade_urbane = data[(data['localizzazione_incidente'] == 1) | (data['localizzazione_incidente'] == 2) | (data['localizzazione_incidente'] == 3)]['veicolo__a___sesso_conducente']
+#strade_extraurbane = data[(data['localizzazione_incidente'] == 4) | (data['localizzazione_incidente'] == 5) | (data['localizzazione_incidente'] == 6)]['veicolo__a___sesso_conducente']
+#autostrade = data[data['localizzazione_incidente'] == 7]['veicolo__a___sesso_conducente']
+#
+#strade_urbane = strade_urbane[strade_urbane != ' '].astype(int)
+#strade_extraurbane = strade_extraurbane[strade_extraurbane != ' '].astype(int)
+#autostrade = autostrade[autostrade != ' '].astype(int)
+#
+#strade_extraurbane = label_utils.join_labels(strade_extraurbane, "dataset/incidenti/Classificazioni/veicolo__a___sesso_conducente.csv").value_counts(normalize=True).sort_index()
+#strade_urbane = label_utils.join_labels(strade_urbane, "dataset/incidenti/Classificazioni/veicolo__a___sesso_conducente.csv").value_counts(normalize=True).sort_index()
+#autostrade = label_utils.join_labels(autostrade, "dataset/incidenti/Classificazioni/veicolo__a___sesso_conducente.csv").value_counts(normalize=True).sort_index()
+#
+#pd.DataFrame(
+#    [strade_extraurbane, strade_urbane, autostrade], 
+#    index=['Autostrade', 'Strade urbane', 'Strade Extra-Urbane']
+#    ).transpose().plot.bar()
+#plt.tight_layout()
+#plt.show()
+
+
+strade_urbane = data[(data['localizzazione_incidente'] == 1) | (data['localizzazione_incidente'] == 2) | (data['localizzazione_incidente'] == 3)]['veicolo__a___et__conducente']
+strade_extraurbane = data[(data['localizzazione_incidente'] == 4) | (data['localizzazione_incidente'] == 5) | (data['localizzazione_incidente'] == 6)]['veicolo__a___et__conducente']
+autostrade = data[data['localizzazione_incidente'] == 7]['veicolo__a___et__conducente']
+
+strade_urbane = strade_urbane[strade_urbane != '  '].astype(int)
+strade_extraurbane = strade_extraurbane[strade_extraurbane != '  '].astype(int)
+autostrade = autostrade[autostrade != '  '].astype(int)
+
+strade_extraurbane = label_utils.join_labels(strade_extraurbane, "dataset/incidenti/Classificazioni/veicolo__a___et__conducente.csv").value_counts(normalize=True).sort_index()
+strade_urbane = label_utils.join_labels(strade_urbane, "dataset/incidenti/Classificazioni/veicolo__a___et__conducente.csv").value_counts(normalize=True).sort_index()
+autostrade = label_utils.join_labels(autostrade, "dataset/incidenti/Classificazioni/veicolo__a___et__conducente.csv").value_counts(normalize=True).sort_index()
+
+pd.DataFrame(
+    [strade_extraurbane, strade_urbane, autostrade], 
+    index=['Autostrade', 'Strade urbane', 'Strade Extra-Urbane']
+    ).transpose().plot()
+plt.tight_layout()
+plt.show()
+
+# TODO: i maggiori incidenti da donne in strade urbane vale per ogni anno?
+
+
