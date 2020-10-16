a7c7a4b85c638af5390f8f1d6c17c9cbb1fca7ba
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-16 16:00:50 +0200

    aggiunta parte di ore incidenti in tesi

--- src/incidenti/incidenti_senza_coords/ore_punta/ore_punta.py
+++ src/incidenti/incidenti_senza_coords/ore_punta/ore_punta.py
@@ -22,7 +22,7 @@ ora_punta_week = ora_punta[ora_punta['giorno_settimana'] < 6]['ora'].value_count
 
 #print(ora_punta_weekend['ora'])
 
-uniti = pd.DataFrame(
+uniti_sera = pd.DataFrame(
     [ora_punta_week, ora_punta_weekend], 
     index=['week', 'weekend']).transpose()
 
@@ -31,14 +31,10 @@ uniti = pd.DataFrame(
 #plt.show()
 
 # Aggiusto per il fatto che la settimana è 5 giorni e il weekend è 2
-uniti = pd.DataFrame([
-    uniti['week'] / 5, 
-    uniti['weekend'] / 2
+uniti_sera = pd.DataFrame([
+    uniti_sera['week'] / 5, 
+    uniti_sera['weekend'] / 2
 ], index=['week', 'weekend']).transpose()
-
-#uniti.plot.bar()
-#plt.xlabel("Incidenti in ore di Punta")
-#plt.show()
 
 # In ogni ora  c'è comunque una differenza di più di 100 incidenti tra week e weekend
 
@@ -48,13 +44,15 @@ ora_punta = data[(data['ora'] > 6) & (data['ora'] < 11)]
 ora_punta_weekend = ora_punta[ora_punta['giorno_settimana'] > 5]['ora'].value_counts().sort_index()
 ora_punta_week = ora_punta[ora_punta['giorno_settimana'] < 6]['ora'].value_counts().sort_index()
 
-#uniti = pd.DataFrame([
-#    ora_punta_week / 5, 
-#    ora_punta_weekend / 2
-#], index=['week', 'weekend']).transpose()
+uniti_mattina = pd.DataFrame([
+    ora_punta_week / 5, 
+    ora_punta_weekend / 2
+], index=['week', 'weekend']).transpose()
 
-#uniti.plot.bar()
-#plt.xlabel("Incidenti in ore di Punta")
+#plt.figure(1)
+#plt.bar(uniti_sera.columns, uniti_sera)
+#plt.figure(2)
+#plt.bar(uniti_mattina.columns, uniti_mattina)
 #plt.show()
 
 # Si nota ancora di  più la differenza tra le 8 e le 9 (probabilmente perchè la gente dorme)
@@ -79,3 +77,16 @@ uniti = pd.DataFrame([
 
 # Si nota differenza anche alle 7, anche se su taglia di campione molto piccola...
 
+ora_week = data[data['giorno_settimana'] < 6]
+ora_weekend = data[data['giorno_settimana'] > 5]#['ora'].value_counts().sort_index()
+
+ora_week = ora_week[ora_week['provincia'] == 15]['ora'].value_counts().sort_index()
+ora_weekend = ora_weekend[ora_weekend['provincia'] == 15]['ora'].value_counts().sort_index()
+
+uniti = pd.DataFrame([
+    ora_week / 5, 
+    ora_weekend / 2
+], index=['week', 'weekend']).transpose()
+
+uniti.plot()
+plt.show()
