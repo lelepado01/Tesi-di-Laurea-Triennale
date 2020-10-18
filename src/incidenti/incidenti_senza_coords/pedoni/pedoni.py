c88033bc9967bf4754a132409411c6e5759b0661
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-18 11:26:29 +0200

    aggiunto grafo con età pedoni feriti e morti

--- src/incidenti/incidenti_senza_coords/pedoni/pedoni.py
+++ src/incidenti/incidenti_senza_coords/pedoni/pedoni.py
@@ -1,6 +1,9 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
+sys.path.append('src')
+import label_utils
 
 path = "dataset/incidenti/incidenti_2011.txt"
 
@@ -8,19 +11,23 @@ data = pd.read_csv(path, sep="\t")
 
 fields = ['pedone_morto_1__sesso']#,  'pedone_morto_2__sesso', 'pedone_morto_3__sesso', 'pedone_morto_4__sesso']
 pedoni_morti = data[fields]
-pedoni_morti = pedoni_morti[pedoni_morti != ' '].value_counts()
+pedoni_morti = pedoni_morti[pedoni_morti != ' ']
+pedoni_morti = label_utils.join_labels(pedoni_morti, 'dataset/incidenti/Classificazioni/pedone_ferito_1__sesso.csv').value_counts()
 
 #print(pedoni_morti['pedone_morto_1__sesso'].unique())
 
 pedoni_feriti = data['pedone_ferito_1__sesso']
-pedoni_feriti = pedoni_feriti[pedoni_feriti != ' '].value_counts()
+pedoni_feriti = pedoni_feriti[pedoni_feriti != ' ']
+pedoni_feriti = label_utils.join_labels(pedoni_morti, 'dataset/incidenti/Classificazioni/pedone_ferito_1__sesso.csv').value_counts()
 
 #plt.subplot(121)
 #plt.pie(pedoni_feriti)
-#plt.title("Pedoni Feriti")
+#plt.title("Sesso Pedoni Feriti")
 #plt.subplot(122)
 #plt.pie(pedoni_morti)
-#plt.title("Pedoni Morti")
+#plt.legend(('Maschi', 'Femmine'))
+#plt.tight_layout()
+#plt.title("Sesso Pedoni Morti")
 #plt.show()
 
 # Mi aspetto 50%, 50%
@@ -46,26 +53,46 @@ def get_people(dataset : pd.DataFrame, campi):
 
     return pd.Series(res)
 
-import sys
-sys.path.append('src')
-import label_utils
-
-fields.remove('intersezione_o_non_interse1')
-pedoni_feriti = get_people(incidenti_pedoni, fields)
-incidenti_pedoni = pd.DataFrame([incidenti_pedoni['intersezione_o_non_interse1'], pedoni_feriti], ['tipo_incrocio', 'pedoni_feriti']).transpose()
-incidenti_pedoni = incidenti_pedoni[incidenti_pedoni['pedoni_feriti'] != 0].value_counts()
-incidenti_pedoni = incidenti_pedoni[incidenti_pedoni > 50]
+#fields.remove('intersezione_o_non_interse1')
+#pedoni_feriti = get_people(incidenti_pedoni, fields)
+#incidenti_pedoni = pd.DataFrame([incidenti_pedoni['intersezione_o_non_interse1'], pedoni_feriti], ['tipo_incrocio', 'pedoni_feriti']).transpose()
+#incidenti_pedoni = incidenti_pedoni[incidenti_pedoni['pedoni_feriti'] != 0].value_counts()
+#incidenti_pedoni = incidenti_pedoni[incidenti_pedoni > 50]
 #incidenti_labels = label_utils.join_labels(incidenti_pedoni['tipo_incrocio'], 'dataset/incidenti/Classificazioni/intersezione_o_non_interse3.csv')
 #incidenti_pedoni = pd.DataFrame([incidenti_labels, incidenti_pedoni], ['tipo_incrocio', 'pedoni_feriti']).transpose().value_counts()
 #print(incidenti_pedoni)
 
-legend = pd.read_csv('dataset/incidenti/Classificazioni/intersezione_o_non_interse3.csv')
-legend = legend['Descrizione']
-
-incidenti_pedoni.plot.bar()
-plt.tight_layout()
-plt.xlabel("")
-#plt.legend((1, 2, 3, 7, 8), ('Incrocio', 'Rotatoria', 'Intersez. segnalata', 'Rettilineo', 'Curva'))
-plt.show()
+#legend = pd.read_csv('dataset/incidenti/Classificazioni/intersezione_o_non_interse3.csv')
+#legend = legend['Descrizione']
+#
+#incidenti_pedoni.plot.bar()
+#plt.tight_layout()
+#plt.xlabel("")
+##plt.legend((1, 2, 3, 7, 8), ('Incrocio', 'Rotatoria', 'Intersez. segnalata', 'Rettilineo', 'Curva'))
+#plt.show()
 
 # Realizza Legenda esterna
+
+# Età dei pedoni feriti?
+
+pedoni_morti = data['pedone_morto_1__et_']
+pedoni_morti = pedoni_morti[pedoni_morti != '  '].astype(int)
+pedoni_morti = label_utils.join_labels(pedoni_morti, 'dataset/incidenti/Classificazioni/veicolo__a___et__conducente.csv')
+pedoni_morti = pedoni_morti.value_counts().sort_index()
+
+pedoni_feriti = data['pedone_ferito_1__et_']
+pedoni_feriti = pedoni_feriti[pedoni_feriti != '  '].astype(int)
+pedoni_feriti = label_utils.join_labels(pedoni_feriti, 'dataset/incidenti/Classificazioni/veicolo__a___et__conducente.csv')
+pedoni_feriti = pedoni_feriti.value_counts().sort_index()
+
+plt.subplot(121)
+plt.tight_layout()
+plt.plot(pedoni_feriti)
+plt.xticks(rotation=90)
+plt.title("Età Pedoni Feriti")
+plt.subplot(122)
+plt.tight_layout()
+plt.xticks(rotation=90)
+plt.plot(pedoni_morti)
+plt.title("Età Pedoni Morti")
+plt.show()
