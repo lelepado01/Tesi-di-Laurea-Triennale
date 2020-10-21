46e31a86290538894420d47ed0d8b992fb8f5e3c
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-21 10:12:25 +0200

    bubble chart delle autostrade manca la legenda

--- src/incidenti/incidenti_aci/autostrade/ore_autostrade.py
+++ src/incidenti/incidenti_aci/autostrade/ore_autostrade.py
@@ -2,12 +2,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def sum_columns(data : pd.DataFrame, normalize = False) -> pd.Series: 
+def sum_columns(data : pd.DataFrame, normalize = False, name=None) -> pd.Series: 
     res = {}
     for col in data.columns: 
         res[col] = sum(data[col])
 
-    res = pd.Series(res).transpose()
+    if name is None:
+        res = pd.Series(res).transpose()
+    else: 
+        res = pd.Series(res, name=name)
 
     if normalize: 
         res = res / sum(res)
@@ -15,8 +18,42 @@ def sum_columns(data : pd.DataFrame, normalize = False) -> pd.Series:
     return res
 
 
-ore_2012 = pd.read_csv('/Users/gabrielepadovani/Desktop/Università/Tesi/dataset/incidenti/aci/autostrade/ore_2012.csv')
-print(ore_2012.columns)
-ore = sum_columns(ore_2012[['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24']])
-ore.plot.bar()
+data = pd.read_csv('dataset/incidenti/aci/autostrade/ore_2012.csv')
+ore = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24']
+#ore_2012 = data
+#print(ore_2012.columns)
+#ore = sum_columns(ore_2012[ore])
+#ore.plot.bar()
+#plt.show()
+
+# Conferma i risultati istat
+
+#print(len(data[data['PROVINCIA'] == 'Milano']['NOME STRADA'].unique()))
+milano = data[data['PROVINCIA'] == 'Milano']
+
+incidenti = pd.DataFrame()
+
+for field in milano['NOME STRADA'].unique(): 
+    incidenti = incidenti.append(
+        sum_columns(milano[milano['NOME STRADA'] == field][ore], name=field), 
+    )
+
+#print(incidenti)
+#incidenti.transpose().plot()
+#plt.legend("")
+#plt.show()
+
+import geopandas as gp
+import contextily as cx
+
+strade = gp.read_file("dataset/incidenti/aci/autostrade/posizione_autostrade.geojson").to_crs(epsg=3857)
+strade.index = strade['name']
+#print(len(strade))
+
+strade = gp.GeoDataFrame(incidenti['18'], geometry=strade['geometry'])
+#print(strade)
+
+ax = strade.plot(figsize=(11,9), markersize=strade['18'] * 100, alpha=0.5, color='orange')
+plt.ylim((5.68 * pow(10, 6), 5.72 * pow(10,6)))
+cx.add_basemap(ax=ax)
 plt.show()
