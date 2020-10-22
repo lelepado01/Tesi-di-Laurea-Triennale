67e72c6efbc413137e85ac918a963570e3feffaa
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-22 16:05:52 +0200

    refactoring: spostato funzioni in utils

--- src/aci_utils.py
+++ src/aci_utils.py
@@ -27,3 +27,45 @@ def sum_field_by_column(data : pd.DataFrame, select_field : str, column_to_sum :
         )
     
     return res
+
+def two_cols_unique(data) -> list: 
+    res = []
+
+    field1 = data.columns[0]
+    field2 = data.columns[1]
+
+    for d1, d2 in zip(data[field1], data[field2]):
+        if not (d1, d2) in res: 
+            res.append((d1, d2))
+
+    return res
+
+
+def filter_with(data : pd.DataFrame, field, comb : str) -> pd.DataFrame: 
+    return data[data[field] == comb]
+
+
+def sum_field_by_two_columns(data : pd.DataFrame, field1 : str, field2 : str, field_to_sum : str) -> pd.DataFrame: 
+    res = pd.DataFrame()
+
+    for combination in two_cols_unique(data[[field1, field2]]): 
+        filtered_data = filter_with(filter_with(data, field1, combination[0]), field2, combination[1])
+        d = {
+            (field1 + "_" + field2) : combination,
+            'Value' : sum(filtered_data[field_to_sum])
+        }
+        res = res.append(d, ignore_index=True)
+
+    return res
+
+
+def filter_by_value(data : pd.Series, value : str, inverted = False) -> pd.Series:
+    res = [] 
+    for d in data: 
+        if inverted: 
+            res.append(value not in str(d))
+        else: 
+            res.append(value in str(d))
+
+    return pd.Series(res)
+
--- src/incidenti/incidenti_aci/agosto/agosto.py
+++ src/incidenti/incidenti_aci/agosto/agosto.py
@@ -1,113 +1,40 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
+sys.path.append('src')
+import aci_utils
 
 path = "dataset/incidenti/aci/autostrade/mesi_"
 
-def two_cols_unique(data) -> list: 
-    res = []
-
-    field1 = data.columns[0]
-    field2 = data.columns[1]
-
-    for d1, d2 in zip(data[field1], data[field2]):
-        if not (d1, d2) in res: 
-            res.append((d1, d2))
-
-    return res
-
-
-def filter_with(data, field, comb : str) -> pd.DataFrame: 
-    return data[data[field] == comb]
-
-
-def sum_field(data): 
-    res = pd.DataFrame()
-
-    for combination in two_cols_unique(data[['NOME STRADA', 'Anno']]): 
-        filtered_data = filter_with(filter_with(data, 'NOME STRADA', combination[0]), 'Anno', combination[1])
-        d = {
-            'Nome e Anno' : combination,
-            'Inc' : sum(filtered_data['Agosto'])
-        }
-        res = res.append(d, ignore_index=True)
-
-    return res
-
-def f(data, key : str, inverted = False):
-    res = [] 
-    for d in data: 
-        if inverted: 
-            res.append(key not in str(d))
-        else: 
-            res.append(key in str(d))
-
-    return pd.Series(res)
-
-
 agosto = pd.DataFrame()
 
-for year in range(2011, 2019):
+for year in range(2011, 2012):
     data = pd.read_csv(path + str(year) + ".csv")
     data['Anno'] = [year] * len(data)
     agosto = agosto.append(data[['NOME STRADA', 'Anno', 'Agosto']])
 
 
-#print(two_cols_unique(data[['NOME STRADA', 'Anno']]))
-#agosto = sum_field(agosto)
-#agosto = agosto[f(agosto['Nome e Anno'], '2018')]
-#agosto = agosto.sort_values(by='Inc', ascending=False).head(20)
+#print(aci_utils.two_cols_unique(data[['NOME STRADA', 'Anno']]))
+agosto = aci_utils.sum_field_by_two_columns(agosto, 'NOME STRADA', 'Anno', 'Agosto')
+agosto = agosto[aci_utils.filter_by_value(agosto['NOME STRADA_Anno'], '2011')]
+agosto = agosto.sort_values(by='Value', ascending=False).head(20)
 
-#print(agosto)
+print(agosto)
 
 #plt.barh(agosto['Nome e Anno'].astype(str), agosto['Inc'])
 #plt.xticks(rotation=90)
 #plt.tight_layout()
 #plt.show()
 
-import sys
-sys.path.append('src')
-import aci_utils
+#agosto_2018 = pd.read_csv(path + "2014.csv")
+#agosto_2018 = agosto_2018[['NOME STRADA', 'Agosto']]
+#agosto_2018 = aci_utils.sum_field_by_column(agosto_2018, 'NOME STRADA', 'Agosto')
+#agosto_2018 = agosto_2018.sort_values(by='val', ascending=False).head(15)
 
-agosto_2018 = pd.read_csv(path + "2014.csv")
-agosto_2018 = agosto_2018[['NOME STRADA', 'Agosto']]
-agosto_2018 = aci_utils.sum_field_by_column(agosto_2018, 'NOME STRADA', 'Agosto')
-agosto_2018 = agosto_2018.sort_values(by='val', ascending=False).head(15)
+#agosto_2018.plot.barh()
+#plt.tight_layout()
+#plt.legend("")
+#plt.show()
 
-agosto_2018.plot.barh()
-plt.tight_layout()
-plt.legend("")
-plt.show()
-
-agosto_2018 = pd.read_csv(path + "2013.csv")
-agosto_2018 = agosto_2018[['NOME STRADA', 'Agosto']]
-agosto_2018 = aci_utils.sum_field_by_column(agosto_2018, 'NOME STRADA', 'Agosto')
-agosto_2018 = agosto_2018.sort_values(by='val', ascending=False).head(15)
-
-agosto_2018.plot.barh()
-plt.tight_layout()
-plt.legend("")
-plt.show()
-
-agosto_2018 = pd.read_csv(path + "2012.csv")
-agosto_2018 = agosto_2018[['NOME STRADA', 'Agosto']]
-agosto_2018 = aci_utils.sum_field_by_column(agosto_2018, 'NOME STRADA', 'Agosto')
-agosto_2018 = agosto_2018.sort_values(by='val', ascending=False).head(15)
-
-agosto_2018.plot.barh()
-plt.tight_layout()
-plt.legend("")
-plt.show()
-
-agosto_2018 = pd.read_csv(path + "2011.csv")
-agosto_2018 = agosto_2018[['NOME STRADA', 'Agosto']]
-agosto_2018 = aci_utils.sum_field_by_column(agosto_2018, 'NOME STRADA', 'Agosto')
-agosto_2018 = agosto_2018.sort_values(by='val', ascending=False).head(15)
-
-agosto_2018.plot.barh()
-plt.tight_layout()
-plt.legend("")
-plt.show()
-
-
-
+# TODO: aggiungere a tesi
