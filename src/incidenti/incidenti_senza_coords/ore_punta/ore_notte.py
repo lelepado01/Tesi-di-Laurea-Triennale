509c6a892eb1dab07f0c72d9b46f971d7573d241
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-26 09:57:50 +0100

    modifiche grafi

--- src/incidenti/incidenti_senza_coords/ore_punta/ore_notte.py
+++ src/incidenti/incidenti_senza_coords/ore_punta/ore_notte.py
@@ -2,35 +2,33 @@
 # IMPORTANTE: dati su tutta ITALIA
 
 import pandas as pd
-import matplotlib.pyplot as plt
-
-def get_ore_notte(data, provincia=None, label = None): 
-    data = data[data['ora'] != 25]
-    notte = data[(data['ora'] < 7) | (data['ora'] > 22)]
-
-    if not provincia is None:
-        notte = notte[notte['provincia'] == provincia]
-
-    ora_notte_weekend = notte[notte['giorno_settimana'] > 5]['ora'].value_counts().sort_index()
-    ora_notte_week = notte[notte['giorno_settimana'] < 6]['ora'].value_counts().sort_index()
-
-    uniti = pd.DataFrame([
-        ora_notte_week / 5, 
-        ora_notte_weekend / 2
-    ], index=['week', 'weekend']).transpose()
-
-    uniti.plot.bar()
-    if not label is None:
-        plt.xlabel(label)
-    plt.show()
+import matplotlib.pyplot as plt    
 
 path = "dataset/incidenti/incidenti_2010.txt"
 
-data = pd.read_csv(path, sep="\t")
+bar_width = 0.8
 
-#get_ore_notte(data, label="Ore Notte")
-get_ore_notte(data, 15, label="Ore Notte Milano")
-get_ore_notte(data, 99, label="Ore Notte Rimini")
+data = pd.read_csv(path, sep="\t")
+data = data[data['ora'] != 25]
+
+notte = data[(data['ora'] < 7) | (data['ora'] > 22)]
+ora_notte_week = notte[notte['giorno_settimana'] < 6]['ora'].value_counts().sort_index()
+ora_notte_weekend = notte[notte['giorno_settimana'] > 5]['ora'].value_counts().sort_index()
+
+ora_notte_week = ora_notte_week.reindex([23,24,1,2,3,4,5,6])
+ora_notte_weekend = ora_notte_weekend.reindex([23,24,1,2,3,4,5,6])
+
+ora_notte_week /= 5 * 52 
+ora_notte_weekend /= 2 * 52
+
+ora_notte_weekend.plot.bar(color='#6262d1', width=bar_width)
+ora_notte_week.plot.bar(color='#d1d162', width=bar_width)
+
+plt.xlabel("Principali ore della notte")
+plt.ylabel("Incidenti per giorno")
+plt.legend(['week', 'weekend'])
+
+plt.show()
 
 # Prendendo solo gli incidenti in milano, il grafo coincide
 # Non cambia molto in altre località... ma la quantità di dati non è molta
