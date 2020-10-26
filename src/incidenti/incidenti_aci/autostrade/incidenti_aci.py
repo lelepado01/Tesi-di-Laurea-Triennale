9b992b1471adf9678cad812f671dd6672f45fb94
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-26 17:22:29 +0100

    aggiunto dataset con le principali autostrade in italia

--- src/incidenti/incidenti_aci/autostrade/incidenti_aci.py
+++ src/incidenti/incidenti_aci/autostrade/incidenti_aci.py
@@ -2,18 +2,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# I dati sulle strade provinciali sono meno...
-
-data = pd.read_csv("dataset/incidenti/aci/strade_provinciali/aci_2018.csv")
-#print(data.columns)
-
-print(data[data['provincia'] == 'Milano']['nome strada'].value_counts())
-
 # I dati sulle autostrade hanno anche mese, ora e giorno della settimana, 
 # con la posizione in quale autostrada
 
 data_autostrade = pd.read_csv("dataset/incidenti/aci/autostrade/localizzazione_2018.csv")
 
-print(data_autostrade['NOME STRADA'].value_counts())
-
-# PROBLEMA: i dati sono già elaborati
+print(data_autostrade['NOME STRADA'].value_counts().sort_values(ascending=False).head(20))
