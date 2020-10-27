bda3dba1d9e7832cd74b1e387def659f2fdedb99
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-27 11:48:32 +0100

    aggiunta mappa autostrade

--- src/incidenti/incidenti_aci/autostrade/incidenti_aci.py
+++ src/incidenti/incidenti_aci/autostrade/incidenti_aci.py
@@ -7,15 +7,19 @@ import matplotlib.pyplot as plt
 
 data_autostrade = pd.read_csv("dataset/incidenti/aci/autostrade/localizzazione_2018.csv")
 
+aut = data_autostrade['NOME STRADA'].value_counts().sort_values(ascending=False).head(20)
 #print(data_autostrade['NOME STRADA'].value_counts().sort_values(ascending=False).head(20))
 
 import geopandas as gp
 import contextily as cx
 
 autostrade = gp.read_file("dataset/autostrade/autostrade.geojson").to_crs(epsg=3857)
-# print(autostrade)
+#print(aut)
 
-ax = autostrade.plot(figsize=(11,9))
+df = pd.merge(autostrade, aut, left_on='name', right_on=aut.index)
+#print(df[['name', 'geometry']])
+
+ax = df.plot(figsize=(11,9), column='NOME STRADA')
 plt.ylim((4.5 * pow(10, 6), 6.1 * pow(10, 6)))
 plt.xlim(left=0.55 * pow(10,6))
 cx.add_basemap(ax=ax, source=cx.providers.CartoDB.Voyager)
