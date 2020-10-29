9f102eb03537a0ddefbbaf6eec6cf228130d7295
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-29 12:42:12 +0100

    aggiunta heatmap dei trimestri

--- src/incidenti/incidenti_senza_coords/mese_incidenti/trimestri.py
+++ src/incidenti/incidenti_senza_coords/mese_incidenti/trimestri.py
@@ -9,8 +9,10 @@ incidenti_2010 = pd.read_csv("dataset/incidenti/incidenti_2010.txt", sep="\t")
 incidenti_2011 = pd.read_csv("dataset/incidenti/incidenti_2011.txt", sep="\t")
 incidenti_2012 = pd.read_csv("dataset/incidenti/incidenti_2012.txt", sep="\t")
 incidenti_2013 = pd.read_csv("dataset/incidenti/incidenti_2013.txt", sep="\t")
+incidenti_2014 = pd.read_csv("dataset/incidenti/incidenti_2014.txt", sep="\t")
 incidenti_2015 = pd.read_csv("dataset/incidenti/incidenti_2015.txt", sep="\t", encoding='latin1')
 incidenti_2016 = pd.read_csv("dataset/incidenti/incidenti_2016.txt", sep="\t", encoding='latin1')
+incidenti_2017 = pd.read_csv("dataset/incidenti/incidenti_2017.txt", sep="\t", error_bad_lines=False, engine="python")
 incidenti_2018 = pd.read_csv("dataset/incidenti/incidenti_2018.txt", sep="\t", encoding='latin1')
 
 def get_trimestre(data : pd.Series) -> pd.Series: 
@@ -27,23 +29,40 @@ def get_provincia(prov : int) -> pd.DataFrame:
     aosta_2011 = get_trimestre(incidenti_2011[incidenti_2011['provincia'] == prov]['mese'].value_counts().sort_index())
     aosta_2012 = get_trimestre(incidenti_2012[incidenti_2012['provincia'] == prov]['mese'].value_counts().sort_index())
     aosta_2013 = get_trimestre(incidenti_2013[incidenti_2013['provincia'] == prov]['mese'].value_counts().sort_index())
+    aosta_2014 = incidenti_2014[incidenti_2014['provincia'] == prov]['trimestre'].value_counts().sort_index()
     aosta_2015 = incidenti_2015[incidenti_2015['provincia'] == prov]['trimestre'].value_counts().sort_index()
     aosta_2016 = incidenti_2016[incidenti_2016['provincia'] == prov]['trimestre'].value_counts().sort_index()
+    aosta_2017 = incidenti_2017[incidenti_2017['provincia'] == prov]['trimestre'].value_counts().sort_index()
     aosta_2018 = incidenti_2018[incidenti_2018['provincia'] == prov]['trimestre'].value_counts().sort_index()
 
-    return pd.DataFrame([aosta_2010, aosta_2011, aosta_2012, aosta_2013, aosta_2015, aosta_2016, aosta_2018], ['2010', '2011', '2012', '2013', '2015', '2016', '2018']).transpose()
+    return pd.DataFrame(
+        [aosta_2010, aosta_2011, aosta_2012, aosta_2013, aosta_2014, aosta_2015, aosta_2016, aosta_2017, aosta_2018], 
+        ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018']
+        )#.transpose()
 
-provs = get_provincia(7)
+provs = get_provincia(15)
 
 index = 0
 for giorni in giorni_al_trimestre: 
     provs.iloc[index] /= giorni
     index += 1
 
-provs.plot(linewidth=1)
-plt.xticks([1,2,3,4])
-plt.legend(bbox_to_anchor=(1,1), loc="upper left")
+import sys
+sys.path.append('src')
+import heatmap as H
+
+fig, ax = plt.subplots()
+
+im, cbar = H.heatmap(provs, provs.index, [1,2,3,4], ax=ax, cmap="YlGn", cbarlabel="Incidenti al trimestre")
+
 plt.xlabel("Trimestre")
-plt.ylabel("Incidenti al giorno in Valle d'Aosta")
-plt.tight_layout()
+fig.tight_layout()
 plt.show()
+
+# provs.plot(linewidth=1)
+# plt.xticks([1,2,3,4])
+# plt.legend(bbox_to_anchor=(1,1), loc="upper left")
+# plt.xlabel("Trimestre")
+# plt.ylabel("Incidenti al giorno")
+# plt.tight_layout()
+# plt.show()
