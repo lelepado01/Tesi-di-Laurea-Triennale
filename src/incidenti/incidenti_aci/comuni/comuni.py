a998601f5ff418700ebc29dec61f34cac210b5a2
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-10-30 12:50:14 +0100

    agginuto grafo province

--- src/incidenti/incidenti_aci/comuni/comuni.py
+++ src/incidenti/incidenti_aci/comuni/comuni.py
@@ -7,14 +7,14 @@ import aci_utils
 
 data = pd.read_csv("dataset/incidenti/aci/autostrade/comuni_2016.csv")
 
-fields = ['INC','IM','MOR','FER']
+fields = ['INC','FER']
 
 #print(len(data['PROVINCIA'].unique()))
 
 # Il numero di feriti dipende dal numero di incidenti?
 # E il numero di morti?
 
-provincia = aci_utils.sum_field_by_column(data, 'PROVINCIA', fields)
+#provincia = aci_utils.sum_field_by_column(data, 'PROVINCIA', fields).transpose()
 # Calcolo coeff. di correlazione
 #print(provincia['FER'].corr(provincia['INC'])) = 0.9826611644228044
 # Il coeff. di Pearson è molto vicino a 1, i due campioni sono strettamente correlati
@@ -22,15 +22,24 @@ provincia = aci_utils.sum_field_by_column(data, 'PROVINCIA', fields)
 #print(provincia['MOR'].corr(provincia['FER'])) = 0.8332572834970239
 # Correlazione più debole, ma comunque presente
 
-#provincia = provincia.head(30)
+df = pd.DataFrame()
+for f in data['PROVINCIA'].unique():
+    df = df.append(pd.DataFrame([f, data[data['PROVINCIA'] == f]['INC'].sum(), data[data['PROVINCIA'] == f]['FER'].sum()]).transpose(), ignore_index=True)
 
-# provincia[['FER', 'INC']].plot()
-# plt.xticks(rotation=90)
-# plt.tight_layout()
-# plt.show()
+df.set_index(df[0], inplace=True)
+df =  df[[1,2]]
+#print(df)
 
-provincia = provincia.sort_values(by='INC', ascending=False).head(10)
-provincia.plot()
+df[1].plot(label='Feriti')
+df[2].plot(label='Incidenti')
+plt.xticks(rotation=90)
+plt.legend()
+plt.tight_layout()
 plt.show()
+
+
+# provincia = provincia.sort_values(by='Inc', ascending=False).head(10)
+# provincia.plot()
+# plt.show()
 
 # IMP: Roma e Milano sono outlier
