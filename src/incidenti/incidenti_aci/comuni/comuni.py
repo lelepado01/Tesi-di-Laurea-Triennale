d9e7b665d7698721a2db38309671db962291bf4d
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-11-06 15:50:55 +0100

    aggiunto codice per correlazione tra incidenti e feriti, rifatto grafo

--- src/incidenti/incidenti_aci/comuni/comuni.py
+++ src/incidenti/incidenti_aci/comuni/comuni.py
@@ -1,20 +1,27 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import sys
-sys.path.append("src")
-import aci_utils
 
-data = pd.read_csv("dataset/incidenti/aci/autostrade/comuni_2016.csv")
+data = pd.read_csv("dataset/incidenti/aci/autostrade/comuni_2018.csv")
 
-fields = ['INC','FER']
+fields = ['INC', 'FER']
 
 #print(len(data['PROVINCIA'].unique()))
 
 # Il numero di feriti dipende dal numero di incidenti?
 # E il numero di morti?
 
-#provincia = aci_utils.sum_field_by_column(data, 'PROVINCIA', fields).transpose()
+def sum_field_by_column(data, select, field_to_sum1, field_to_sum2): 
+    dic = {}
+    for f in data[select].unique(): 
+        dic[f] = [
+            data[data[select] == f][field_to_sum1].sum(), 
+            data[data[select] == f][field_to_sum2].sum()
+            ]
+
+    return pd.DataFrame(dic)
+
+provincia = sum_field_by_column(data, 'PROVINCIA', fields[0], fields[1]).transpose()
 # Calcolo coeff. di correlazione
 #print(provincia['FER'].corr(provincia['INC'])) = 0.9826611644228044
 # Il coeff. di Pearson è molto vicino a 1, i due campioni sono strettamente correlati
@@ -22,8 +29,21 @@ fields = ['INC','FER']
 #print(provincia['MOR'].corr(provincia['FER'])) = 0.8332572834970239
 # Correlazione più debole, ma comunque presente
 
-# provincia = provincia.sort_values(by='Inc', ascending=False).head(10)
-# provincia.plot()
-# plt.show()
+provincia = provincia.sort_values(by=0).head(20).sort_index()
+provincia[0].plot(label='Incidenti')
+provincia[1].plot(label='Feriti')
+plt.legend()
+plt.xticks(range(0,20), provincia.index, rotation=90)
+plt.ylabel("Numero di incidenti e feriti (2018)")
+plt.tight_layout()
+plt.show()
 
 # IMP: Roma e Milano sono outlier
+
+# inc = data['INC']
+# fer = data['FER']
+# mor = data['MOR']
+
+# print(inc.corr(fer))
+# print(inc.corr(mor))
+# print(fer.corr(mor))
