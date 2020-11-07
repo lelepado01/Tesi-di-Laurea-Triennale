cfbd094388dcff531e084535842dbebb2618d2e1
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-11-07 14:33:12 +0100

    aggiunta mappa linee tranviarie e aggiunta a tesi

---
+++ src/tram/tram_milano.py
@@ -0,0 +1,14 @@
+
+import geopandas as gp
+import matplotlib.pyplot as plt
+import contextily as cx
+
+path = "dataset/pave/tram/L010204.Shp"
+
+data = gp.read_file(path).to_crs(epsg=3857)
+
+ax = data.plot()
+cx.add_basemap(ax=ax)
+plt.ylim((5.69 * pow(10, 6), 5.7 * pow(10,6)))
+plt.xlim((1.016 * pow(10, 6), 1.0278 * pow(10,6)))
+plt.show()
