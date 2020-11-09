05e03a3fc70c45cfff95a04662b85077557c90ac
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-11-09 10:12:22 +0100

    aggiunto grafo per incidenti in base all'area

--- src/municipi_milano/milano.py
+++ src/municipi_milano/milano.py
@@ -1,5 +1,6 @@
 
 import geopandas as gp
+import pandas as pd
 import contextily as cx
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import legend
@@ -31,9 +32,29 @@ inc = gp.GeoSeries(df).sort_index()
 data.index = data['MUNICIPIO']
 data['Incidenti'] = inc
 
-fig, (ax1, ax2) = plt.subplots(ncols=2, sharex=True, sharey=True)
-layer_m = data.plot(ax = ax1, column='Incidenti', cmap='OrRd', alpha=0.5, legend=True)
-cx.add_basemap(ax=layer_m)
-inc_layer = incidenti.plot(ax=ax2, alpha=0.02)
-cx.add_basemap(ax=inc_layer)
+area = pd.Series(data['AREA'], index=data['MUNICIPIO'])
+# area = area / area.sum()
+incidenti = pd.Series(inc, index=inc.index)
+# print(area)
+# print(incidenti)
+incidenti_per_zona = (pd.Series(incidenti) / area) * 1000000
+#print(incidenti_per_zona)
+
+plt.subplot(1,2,1)
+plt.bar(inc.index, inc, color='#5894dd', width=0.9)
+plt.ylabel("Incidenti all'anno per zona")
+plt.xticks(range(1,10))
+
+plt.subplot(1,2,2)
+plt.bar(incidenti_per_zona.index, incidenti_per_zona, label="incidenti pesati", color='#58d7dd', width=0.9)
+plt.ylabel("Incidenti al chilometro quadrato per zona di Milano")
+plt.xticks(range(1,10))
+
 plt.show()
+
+# fig, (ax1, ax2) = plt.subplots(ncols=2, sharex=True, sharey=True)
+# layer_m = data.plot(ax = ax1, column='Incidenti', cmap='OrRd', alpha=0.5, legend=True)
+# cx.add_basemap(ax=layer_m)
+# inc_layer = incidenti.plot(ax=ax2, alpha=0.02)
+# cx.add_basemap(ax=inc_layer)
+# plt.show()
