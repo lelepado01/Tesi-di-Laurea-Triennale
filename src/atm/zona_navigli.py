fb0e8b954dbe52b35f6689aa94758b72b193f486
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-11-10 11:02:17 +0100

    aggiunto grafo e codice per zona navigli

--- src/atm/zona_navigli.py
+++ src/atm/zona_navigli.py
@@ -13,9 +13,9 @@ bounds = [1.020 * scale, 1.025 * scale, 5.690 * scale, 5.696 * scale]
 data = gp.read_file("dataset/zone_milano/zone.geojson").to_crs(epsg=3857)
 
 autobus = data[data['name'] == "Navigli Autobus"]
-# ax = autobus.plot(alpha=0.5, color='orange')
-
 street = data[data['name'] == "Navigli Incidenti"]
+
+# ax = autobus.plot(alpha=0.5, color='orange')
 # ax2 = street.plot(ax=ax, alpha=0.5, color='yellow')
 
 # plt.ylim([bounds[2], bounds[3]])
