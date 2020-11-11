2f6e3630c814b7fdc66f558fd36a67516954c1d7
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-11-11 17:06:32 +0100

    modificata mappa con autostrade

--- src/incidenti/incidenti_aci/autostrade/ore_autostrade.py
+++ src/incidenti/incidenti_aci/autostrade/ore_autostrade.py
@@ -44,11 +44,11 @@ for i in range(10,25):
 strade = gp.GeoDataFrame(inc, geometry=strade['geometry'])
 print(strade)
 
-ax = strade.plot(figsize=(11,9), column= 0, alpha=0.5, legend=True)
+ax = strade.plot(figsize=(11,9), column= 0, cmap='hot_r' ,legend=True, linewidth=2)
 plt.ylim((5.68 * pow(10, 6), 5.72 * pow(10,6)))
 plt.xlim((pow(10, 6), 1.04 * pow(10,6)))
 plt.xlabel("Incidenti nelle strade principali a Milano (2018)")
-cx.add_basemap(ax=ax)
+cx.add_basemap(ax=ax, alpha=0.8)
 plt.show()
 
 # fields = [
