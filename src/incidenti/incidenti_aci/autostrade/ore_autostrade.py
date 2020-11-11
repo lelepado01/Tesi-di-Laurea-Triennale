0da89327f8091f18207261da19f5ab27d3374bb5
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-11-11 14:46:46 +0100

    aggiunto linechart di incidenti a milano

--- src/incidenti/incidenti_aci/autostrade/ore_autostrade.py
+++ src/incidenti/incidenti_aci/autostrade/ore_autostrade.py
@@ -20,39 +20,32 @@ def sum_columns(data : pd.DataFrame, normalize = False, name=None) -> pd.Series:
 
 data = pd.read_csv('dataset/incidenti/aci/autostrade/ore_2018.csv')
 ore = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24']
-#ore_2012 = data
-#print(ore_2012.columns)
-#ore = sum_columns(ore_2012[ore])
-#ore.plot.bar()
-#plt.show()
 
-# Conferma i risultati istat
-
-#print(len(data[data['PROVINCIA'] == 'Milano']['NOME STRADA'].unique()))
+import geopandas as gp
+import contextily as cx
 milano = data[data['PROVINCIA'] == 'Milano']
 
 incidenti = pd.DataFrame()
-
 for field in milano['NOME STRADA'].unique(): 
     incidenti = incidenti.append(
         sum_columns(milano[milano['NOME STRADA'] == field][ore], name=field), 
     )
 
-#print(incidenti)
-#incidenti.transpose().plot()
-#plt.legend("")
-#plt.show()
-
-import geopandas as gp
-import contextily as cx
-
-strade = gp.read_file("dataset/incidenti/aci/autostrade/posizione_autostrade.geojson").to_crs(epsg=3857)
+strade = gp.read_file("dataset/autostrade/autostrade_milano_linee.geojson").to_crs(epsg=3857)
 strade.index = strade['name']
 
-strade = gp.GeoDataFrame(incidenti[ore], geometry=strade['geometry'])
+print(incidenti)
+inc = incidenti['01']
+for i in ['02', '03', '04', '05', '06', '07', '08', '09']:
+    inc = incidenti[i] + inc
+for i in range(10,25): 
+    inc = incidenti[str(i)] + inc
 
-ax = strade.plot(figsize=(11,9), markersize=strade[ore].sum() * 10, alpha=0.5, color='orange')
+strade = gp.GeoDataFrame(inc, geometry=strade['geometry'])
+
+ax = strade.plot(figsize=(11,9), column= 0, alpha=0.5, legend=True)
 plt.ylim((5.68 * pow(10, 6), 5.72 * pow(10,6)))
+plt.xlim((pow(10, 6), 1.04 * pow(10,6)))
 plt.xlabel("Incidenti nelle strade principali a Milano (2018)")
 cx.add_basemap(ax=ax)
 plt.show()
