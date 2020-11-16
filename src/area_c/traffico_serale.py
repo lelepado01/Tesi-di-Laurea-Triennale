736780b25020cadd18487c1729920a1dd1f679f7
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-11-16 10:22:13 +0100

    stimato traffico nelle ore serali in generale

--- src/area_c/traffico_serale.py
+++ src/area_c/traffico_serale.py
@@ -1,3 +1,33 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime
+
+data = pd.read_csv("dataset/area_c/orari_2016.csv", sep=';').dropna()
+
+orari_serali = data[(data['hour'] == 23) | (data['hour'] == 24) |(data['hour'] == 0) | (data['hour'] == 1) | (data['hour'] == 2) | (data['hour'] == 3) | (data['hour'] == 4) | (data['hour'] == 5) | (data['hour'] == 6)]
+
+def is_weekend(year, month, day) -> pd.Series: 
+    s = pd.Series()
+
+    for y, m, d in zip(year, month, day): 
+        s = s.append(pd.Series(datetime.datetime(y, m, d).weekday() > 4), ignore_index=True)
+
+    return s
+
+# print(len(orari_serali))
+
+weekend_days = orari_serali
+
+weekend_days = orari_serali[is_weekend(orari_serali['year'], orari_serali['month'], orari_serali['day'])]
+
+# print(weekend_days)
+
+traffico = {}
+for f in orari_serali['hour'].unique():
+    traffico[f] = orari_serali[orari_serali['hour'] == f]['totale'].sum()
+
+traffico = pd.DataFrame(traffico, index=['incidenti']).transpose()
+
+traffico.plot.bar()
+plt.show()
--- src/incidenti/incidenti_senza_coords/ore_punta/ore_notte.py
+++ src/incidenti/incidenti_senza_coords/ore_punta/ore_notte.py
@@ -4,16 +4,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt    
 
-path = "dataset/incidenti/incidenti_2010.txt"
+path = "dataset/incidenti/incidenti_2016.txt"
 
 bar_width = 0.96
 
-data = pd.read_csv(path, sep="\t")
-data = data[data['ora'] != 25]
+data = pd.read_csv(path, sep="\t", encoding='latin1')
+data = data[data['Ora'] != 25]
 
-notte = data[(data['ora'] < 7) | (data['ora'] > 22)]
-ora_notte_week = notte[notte['giorno_settimana'] < 6]['ora'].value_counts().sort_index()
-ora_notte_weekend = notte[notte['giorno_settimana'] > 5]['ora'].value_counts().sort_index()
+notte = data[(data['Ora'] < 7) | (data['Ora'] > 22)]
+ora_notte_week = notte[notte['giorno'] < 6]['Ora'].value_counts().sort_index()
+ora_notte_weekend = notte[notte['giorno'] > 5]['Ora'].value_counts().sort_index()
 
 ora_notte_week = ora_notte_week.reindex([23,24,1,2,3,4,5,6])
 ora_notte_weekend = ora_notte_weekend.reindex([23,24,1,2,3,4,5,6])
@@ -30,7 +30,7 @@ pd.DataFrame(
 # ora_notte_week.plot.bar(color='#', width=bar_width)
 
 plt.xlabel("Principali ore della notte")
-plt.ylabel("Incidenti per giorno")
+plt.ylabel("Incidenti per giorno (2016)")
 plt.legend(['week', 'weekend'])
 
 plt.show()
