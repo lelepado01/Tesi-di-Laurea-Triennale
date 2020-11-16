6e25e968a129611cd2bf03cd90da83825a22a6d9
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-11-16 09:39:49 +0100

    aggiornati grafi su incidenti in week/weekend per 2018 invece di 2010

--- src/incidenti/incidenti_senza_coords/ore_punta/week_weekend.py
+++ src/incidenti/incidenti_senza_coords/ore_punta/week_weekend.py
@@ -2,13 +2,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-path = "dataset/incidenti/incidenti_2010.txt"
+path = "dataset/incidenti/incidenti_2018.txt"
 
 data = pd.read_csv(path, sep="\t")
-data = data[data['ora'] != 25]
+data = data[data['Ora'] != 25]
 
-ora_weekend = data[data['giorno_settimana'] > 5]['ora'].value_counts().sort_index()
-ora_week = data[data['giorno_settimana'] < 6]['ora'].value_counts().sort_index()
+ora_weekend = data[data['giorno'] > 5]['Ora'].value_counts().sort_index()
+ora_week = data[data['giorno'] < 6]['Ora'].value_counts().sort_index()
 
 ora_week /= 5 * 52
 ora_weekend /= 2 * 52
@@ -16,14 +16,14 @@ ora_weekend /= 2 * 52
 ora_week.plot(color='#d5ef5f', alpha=0.5, label="Week")
 ora_weekend.plot(color='#7057e0', alpha=0.5, label="Weekend")
 plt.xlabel("Orario")
-plt.ylabel("Numero di incidenti per giorno (2010)")
+plt.ylabel("Numero di incidenti per giorno (2018)")
 plt.legend()
 plt.fill_between(ora_week.index, ora_week, color='#d5ef5f', alpha=0.5)
 plt.fill_between(ora_weekend.index, ora_weekend, color='#7057e0', alpha=0.5)
 plt.show()
 
-# print((ora_week[18] / ora_week.mean()) * 100 -100)
-# print((ora_weekend[18] / ora_weekend.mean()) * 100 -100)
+print((ora_week[18] / ora_week.mean()) * 100 -100)
+print((ora_weekend[18] / ora_weekend.mean()) * 100 -100)
 
 # domenica = data[data['giorno_settimana'] == 6]['ora'].value_counts().sort_index()
 
--- src/incidenti/incidenti_senza_coords/ore_punta/week_weekend_milano.py
+++ src/incidenti/incidenti_senza_coords/ore_punta/week_weekend_milano.py
@@ -4,16 +4,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-path = "dataset/incidenti/incidenti_2010.txt"
+path = "dataset/incidenti/incidenti_2018.txt"
 
 data = pd.read_csv(path, sep="\t")
-data = data[data['ora'] != 25]
+data = data[data['Ora'] != 25]
 
-ora_week = data[data['giorno_settimana'] < 6]
-ora_weekend = data[data['giorno_settimana'] > 5]
+ora_week = data[data['giorno'] < 6]
+ora_weekend = data[data['giorno'] > 5]
 
-ora_week = ora_week[ora_week['provincia'] == 15]['ora'].value_counts().sort_index()
-ora_weekend = ora_weekend[ora_weekend['provincia'] == 15]['ora'].value_counts().sort_index()
+ora_week = ora_week[ora_week['provincia'] == 15]['Ora'].value_counts().sort_index()
+ora_weekend = ora_weekend[ora_weekend['provincia'] == 15]['Ora'].value_counts().sort_index()
 
 ora_week /= 5 * 52
 ora_weekend /= 2 * 52
@@ -21,7 +21,7 @@ ora_weekend /= 2 * 52
 ora_week.plot(color='#d5ef5f', alpha=0.5, label="Week")
 ora_weekend.plot(color='#7057e0', alpha=0.5, label="Weekend")
 plt.xlabel("Orario")
-plt.ylabel("Numero di incidenti per giorno (2010)")
+plt.ylabel("Numero di incidenti per giorno (2018)")
 plt.legend()
 plt.fill_between(ora_week.index, ora_week, color='#d5ef5f', alpha=0.5)
 plt.fill_between(ora_weekend.index, ora_weekend, color='#7057e0', alpha=0.5)
@@ -30,6 +30,6 @@ plt.show()
 print((ora_week[8] / ora_week.mean()) * 100 -100)
 print((ora_weekend[8] / ora_weekend.mean()) * 100 -100)
 
-domenica = data[data['giorno_settimana'] == 6]['ora'].value_counts().sort_index()
+domenica = data[data['giorno'] == 6]['Ora'].value_counts().sort_index()
 
 print((domenica[8] / domenica.mean()) * 100 -100)
