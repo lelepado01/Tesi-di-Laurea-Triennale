b86424f3a2155c93440b2367c390899cd64a2d80
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-11-20 11:02:59 +0100

    aggiunto esperimento di grafo

--- src/incidenti/incidenti_senza_coords/localizzazione_incidente/intersezioni.py
+++ src/incidenti/incidenti_senza_coords/localizzazione_incidente/intersezioni.py
@@ -20,8 +20,50 @@ intersezione_labels = label_utils.join_labels(
 
 intersezione_labels = intersezione_labels[intersezione_labels > 200]
 
+intersezione_labels /= intersezione_labels.sum()
+
+######################
+
+data = pd.read_csv("dataset/incidenti/incidenti_2018.txt", sep="\t", encoding='latin1')
+
+incr = 'intersezione_o_non_interse3'
+morti = ['morti_entro_24_ore', 'morti_entro_30_giorni']
+
+data['morti'] = data[morti[0]] + data[morti[1]]
+
+df = {}
+for inc in data[incr].unique(): 
+    df[inc] = 0
+
+for inc, morti in zip(data[incr], data['morti']): 
+    df[inc] += morti
+
+indice_mort = pd.Series(df.values(),index =  df.keys()).sort_index()
+
+num_incr = data[incr].value_counts().sort_index()
+
+mortalita = pd.DataFrame([indice_mort * 100 / num_incr], 
+    index=['indice']
+).transpose()
+
+mortalita['incrocio'] = mortalita.index
+
+import sys
+sys.path.append("src")
+import label_utils
+
+mortalita['incrocio'] = label_utils.join_labels(mortalita['incrocio'], "dataset/incidenti/Classificazioni/intersezione_o_non_interse3.csv")
+
+mortalita['indice'] /= mortalita['indice'].sum()
+mortalita.index = mortalita['incrocio']
+mortalita = mortalita['indice']
+
+df = pd.DataFrame([intersezione_labels, mortalita], ['Numero di incidenti', 'Indice di mortalità']).transpose()
+
+######################
+
 # GRAFO 4
-intersezione_labels.plot.barh(color='#92ba4e', width=0.9)
+df.plot.barh(color=['#92ba4e', '#ff0000'], width=0.9, alpha=0.5)
 plt.xlabel("Incidenti all'anno (2018)")
 plt.tight_layout()
 plt.show()
