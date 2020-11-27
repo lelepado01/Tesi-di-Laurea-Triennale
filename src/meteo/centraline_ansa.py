16328e84a7d3ccfb2932dead0664fe6a831d3b41
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-11-27 10:25:31 +0100

    ordinati dati meteo

--- src/meteo/centraline_ansa.py
+++ src/meteo/centraline_ansa.py
@@ -11,8 +11,12 @@ incidenti = gp.read_file(path_incidenti).to_crs(epsg=3857)
 
 # uso gli incidenti in modo da avere la posizione delle centraline 
 # relativamente al centro di milano
+scale = 10**6
 
-layer_sensori = sensori.plot(figsize=(11,9))
+layer_sensori = sensori.plot(figsize=(7,5))
 layer_incidenti = incidenti.plot(ax=layer_sensori, alpha=0.0)
 cx.add_basemap(ax=layer_incidenti)
+plt.axis('off')
+plt.xlim((1.017 * scale, 1.032 * scale))
+plt.ylim((5.69 * scale, 5.705 * scale))
 plt.show()
