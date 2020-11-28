51c12954ac8e7753b32a8204dc347575e7d2bae0
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-11-28 16:28:37 +0100

    modificato grafi

--- src/autovelox/autovelox_2014.py
+++ src/autovelox/autovelox_2014.py
@@ -4,15 +4,25 @@ import matplotlib.pyplot as plt
 import contextily as cx
 
 import sys
+
+from matplotlib.pyplot import legend
 sys.path.append("src")
 
 path = "dataset/autovelox/autovelox_2014.csv"
 autovelox = gp.read_file(path).set_crs(epsg=3857)
 autovelox = gp.GeoDataFrame(geometry=gp.points_from_xy(autovelox['field_1'], autovelox['field_2']))
 
-incidenti = gp.read_file("dataset/incidenti/inc_strad_milano_2016.geojson").to_crs(epsg=3857)
+autovelox_tutti = gp.read_file("dataset/autovelox/autovelox_milano.geojson").to_crs(epsg=3857)
 
-layer_i = incidenti.plot(color='blue', alpha=0.07, figsize=(11,9))
-layer_a = autovelox.plot(ax=layer_i, color='red')
+import matplotlib.patches as mpatches
+patches = [
+    mpatches.Patch(color='red', label='Installazioni nel 2014'), 
+    mpatches.Patch(color='blue', label='Installazioni ignote')
+    ]
+
+layer_i = autovelox_tutti.plot(color='blue',markersize=16, figsize=(7,5))
+layer_a = autovelox.plot(ax=layer_i, color='red',markersize=20)
+plt.legend(handles=patches)
+plt.axis('off')
 cx.add_basemap(ax=layer_a)
 plt.show()
