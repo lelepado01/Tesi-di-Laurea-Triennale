e4c7d83decfa960b01de094ac55a59b91eadbb18
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-12-01 10:06:24 +0100

    aggiunto grafo

--- src/incidenti/incidenti_senza_coords/localizzazione_incidente/rapporto_tipologie.py
+++ src/incidenti/incidenti_senza_coords/localizzazione_incidente/rapporto_tipologie.py
@@ -2,15 +2,17 @@
 from matplotlib.pyplot import colorbar
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 import sys
 sys.path.append("src")
 import label_utils
+import heatmap as H
 
 path = "dataset/incidenti/incidenti_"
 
+tipo_incidenti = ['Frontale-laterale', 'Tamponamento', 'Investimento pedoni', 'Scontro laterale']
+
 inc_per_anno = pd.DataFrame()
-for year in range(2010, 2014): 
+for year in range(2010, 2019): 
     if year == 2017: 
         data = pd.read_csv(path + str(year) + ".txt", sep="\t",  error_bad_lines=False, engine='python')
     else: 
@@ -21,14 +23,22 @@ for year in range(2010, 2014):
     natura_incidente_labels = label_utils.join_labels(
         natura_incidente, 
         "dataset/incidenti/Classificazioni/natura_incidente.csv"
-    ).value_counts(normalize=True).sort_values().tail(3)
+    ).value_counts(normalize=True)
 
-    # print(str(year) + "------------------------")
-    inc_per_anno[str(year)] = np.flip(pd.Series.round(pd.Series(natura_incidente_labels), decimals=3).values)
+    natura_incidente_labels = natura_incidente_labels[tipo_incidenti]
 
-inc_per_anno.index = range(1,4)
+    inc_per_anno[str(year)] = pd.Series(natura_incidente_labels)
 
-plt.imshow(inc_per_anno)
 
-plt.tight_layout()
+fig, ax = plt.subplots()
+
+im, cbar = H.heatmap(
+    inc_per_anno, 
+    inc_per_anno.index, 
+    inc_per_anno.columns, 
+    ax=ax, cmap="OrRd", cbarlabel="Categorie di incidenti più frequenti per anno")
+texts = H.annotate_heatmap(im)
+
+fig.tight_layout()
+
 plt.show()
