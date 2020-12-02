a806b6693dc06c37073e9c38c62e5284f66baf88
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-12-02 09:16:40 +0100

    aggiunto testi a incroci_pedoni e refactoring

--- src/incidenti/incidenti_senza_coords/pedoni/pedoni_incroci.py
+++ src/incidenti/incidenti_senza_coords/pedoni/pedoni_incroci.py
@@ -1,8 +1,6 @@
 
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib
 import sys
 sys.path.append('src')
 import label_utils
@@ -33,161 +31,25 @@ def get_people(dataset : pd.DataFrame, campi):
 
     return pd.Series(res)
 
-
-def heatmap(data, row_labels, col_labels, ax=None,
-            cbar_kw={}, cbarlabel="", **kwargs):
-    """
-    Create a heatmap from a numpy array and two lists of labels.
-
-    Parameters
-    ----------
-    data
-        A 2D numpy array of shape (N, M).
-    row_labels
-        A list or array of length N with the labels for the rows.
-    col_labels
-        A list or array of length M with the labels for the columns.
-    ax
-        A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
-        not provided, use current axes or create a new one.  Optional.
-    cbar_kw
-        A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
-    cbarlabel
-        The label for the colorbar.  Optional.
-    **kwargs
-        All other arguments are forwarded to `imshow`.
-    """
-
-    if not ax:
-        ax = plt.gca()
-
-    # Plot the heatmap
-    im = ax.imshow(data, **kwargs)
-
-    # Create colorbar
-    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
-    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
-
-    # We want to show all ticks...
-    ax.set_xticks(np.arange(data.shape[1]))
-    ax.set_yticks(np.arange(data.shape[0]))
-    # ... and label them with the respective list entries.
-    ax.set_xticklabels(col_labels)
-    ax.set_yticklabels(row_labels)
-
-    # Let the horizontal axes labeling appear on top.
-    ax.tick_params(top=True, bottom=False,
-                   labeltop=True, labelbottom=False)
-
-    # Rotate the tick labels and set their alignment.
-    #plt.setp(ax.get_xticklabels(), rotation=-30, ha="right",
-    #         rotation_mode="anchor")
-
-    # Turn spines off and create white grid.
-    for edge, spine in ax.spines.items():
-        spine.set_visible(False)
-
-    ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
-    ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
-    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
-    ax.tick_params(which="minor", bottom=False, left=False)
-
-    return im, cbar
-
-
-def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
-                     textcolors=["black", "white"],
-                     threshold=None, **textkw):
-    """
-    A function to annotate a heatmap.
-
-    Parameters
-    ----------
-    im
-        The AxesImage to be labeled.
-    data
-        Data used to annotate.  If None, the image's data is used.  Optional.
-    valfmt
-        The format of the annotations inside the heatmap.  This should either
-        use the string format method, e.g. "$ {x:.2f}", or be a
-        `matplotlib.ticker.Formatter`.  Optional.
-    textcolors
-        A list or array of two color specifications.  The first is used for
-        values below a threshold, the second for those above.  Optional.
-    threshold
-        Value in data units according to which the colors from textcolors are
-        applied.  If None (the default) uses the middle of the colormap as
-        separation.  Optional.
-    **kwargs
-        All other arguments are forwarded to each call to `text` used to create
-        the text labels.
-    """
-
-    if not isinstance(data, (list, np.ndarray)):
-        data = im.get_array()
-
-    # Normalize the threshold to the images color range.
-    if threshold is not None:
-        threshold = im.norm(threshold)
-    else:
-        threshold = im.norm(data.max())/2.
-
-    # Set default alignment to center, but allow it to be
-    # overwritten by textkw.
-    kw = dict(horizontalalignment="center",
-              verticalalignment="center")
-    kw.update(textkw)
-
-    # Get the formatter in case a string is supplied
-    if isinstance(valfmt, str):
-        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
-
-    # Loop over the data and create a `Text` for each "pixel".
-    # Change the text's color depending on the data.
-    texts = []
-    for i in range(data.shape[0]):
-        for j in range(data.shape[1]):
-            kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
-            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
-            texts.append(text)
-
-    return texts
-
-
-
 fields.remove('intersezione_o_non_interse1')
 pedoni_feriti = get_people(incidenti_pedoni, fields)
 incidenti_pedoni = pd.DataFrame([incidenti_pedoni['intersezione_o_non_interse1'], pedoni_feriti], ['tipo_incrocio', 'pedoni_feriti']).transpose()
 incidenti_pedoni = incidenti_pedoni[incidenti_pedoni['pedoni_feriti'] != 0]
 
 incidenti_labels = label_utils.join_labels(incidenti_pedoni['tipo_incrocio'], 'dataset/incidenti/Classificazioni/intersezione_o_non_interse3.csv')
-#print(incidenti_labels)
-#print(incidenti_pedoni)
 incidenti_pedoni = pd.DataFrame([incidenti_labels, incidenti_pedoni['pedoni_feriti']], ['tipo_incrocio', 'pedoni_feriti']).transpose()
-# print(pd.crosstab(incidenti_pedoni['tipo_incrocio'], incidenti_pedoni['pedoni_feriti']))
 
 tab = pd.DataFrame(pd.crosstab(incidenti_pedoni['tipo_incrocio'], incidenti_pedoni['pedoni_feriti']))
 
-# fig, ax = plt.subplots(figsize=(11,9))
-
-# ax.imshow(tab)
-# ax.set_yticklabels(tab.index)
-# ax.set_xticklabels([1,2,3,4])
-
-# for i in range(len(tab)):
-#     for j in range(len(tab.columns)):
-#         text = ax.text(j, i, tab.iloc[i, j], ha="center", va="center", color="w")
-
-# ax.set_title("ciao")
-# fig.tight_layout()
-# plt.show()
-
+import sys
+sys.path.append("src")
+import heatmap as H
 
 fig, ax = plt.subplots()
 
-im, cbar = heatmap(tab, tab.index, [1,2,3,4], ax=ax,
+im, cbar = H.heatmap(tab, tab.index, [1,2,3,4], ax=ax,
                    cmap="YlGn", cbarlabel="Incidenti all'anno (2011)")
-texts = annotate_heatmap(im, valfmt="{x}")
+texts = H.annotate_heatmap(im, valfmt="{x}")
 plt.title("Pedoni coinvolti", fontdict = {'fontsize' : 10})
 fig.tight_layout()
 plt.show()
