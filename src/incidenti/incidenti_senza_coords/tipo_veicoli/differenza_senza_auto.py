80e4d9f238c39b88951865c5ad9b25060343c8aa
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-12-04 09:41:12 +0100

    messo a posto una cartella

--- src/incidenti/incidenti_senza_coords/tipo_veicoli/differenza_senza_auto.py
+++ src/incidenti/incidenti_senza_coords/tipo_veicoli/differenza_senza_auto.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append("src/")
 import label_utils
+import heatmap as H
 
 data = pd.read_csv("dataset/incidenti/incidenti_2018.txt", sep="\t")
 
@@ -16,13 +17,18 @@ strade_urbane = label_utils.join_labels(strade_urbane, "dataset/incidenti/Classi
 strade_extraurbane = data[(data['localizzazione_incidente'] == 4) | (data['localizzazione_incidente'] == 5) | (data['localizzazione_incidente'] == 6)]['tipo_veicolo_a']
 strade_extraurbane = label_utils.join_labels(strade_extraurbane, "dataset/incidenti/Classificazioni/tipo_veicoli__b_.csv").value_counts(normalize=True).sort_index()
 
-uniti = pd.DataFrame([
-   autostrade_veicoli, strade_urbane, strade_extraurbane
-], index=['Autostrade', 'Strade urbane', 'Strade Extra-Urbane']).transpose()
+autostrade_veicoli = autostrade_veicoli[autostrade_veicoli.index != 'Auto privata']
+strade_urbane = strade_urbane[strade_urbane.index != 'Auto privata']
+strade_extraurbane = strade_extraurbane[strade_extraurbane.index != 'Auto privata']
 
-uniti[uniti > 0.001].dropna().transpose().plot.barh(width=1)
-plt.xlabel("Percentuale del tipo di veicolo")
-plt.yticks()
-plt.legend(bbox_to_anchor=(1,1), loc="upper left")
-plt.tight_layout()
+df = pd.DataFrame([
+   autostrade_veicoli, strade_urbane, strade_extraurbane
+], index=['Autostrade', 'Strade urbane', 'Strade Extra-Urbane']).transpose().dropna().transpose()
+
+fig, ax = plt.subplots()
+
+im, cbar = H.heatmap(df, df.index, df.columns, ax=ax, cmap="YlGn", cbarlabel="Percentuale del tipo di veicolo (2018)", xticks_rotated=True)
+texts = H.annotate_heatmap(im, valfmt="")
+
+fig.tight_layout()
 plt.show()
