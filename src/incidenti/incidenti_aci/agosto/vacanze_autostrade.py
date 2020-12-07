36a5e6a3dfac3a3d87a8e25448db751ca8a99edf
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-12-07 10:25:09 +0100

    modificato grafo

--- src/incidenti/incidenti_aci/agosto/vacanze_autostrade.py
+++ src/incidenti/incidenti_aci/agosto/vacanze_autostrade.py
@@ -4,25 +4,26 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append('src')
 import aci_utils
+import heatmap as H
 
 path = "dataset/incidenti/aci/autostrade/mesi_"
 
-s = 0
-index = 1
+df = pd.DataFrame()
 for year in range(2011, 2019): 
     agosto_2018 = pd.read_csv(path + str(year)+".csv")
     agosto_2018 = agosto_2018[['NOME STRADA', 'Agosto']]
     agosto_2018 = aci_utils.sum_field_by_column(agosto_2018, 'NOME STRADA', 'Agosto')
     agosto_2018 = agosto_2018.sort_values(by='val', ascending=False).head(5)
-    #print(agosto_2018)
-    s += agosto_2018['val'].iloc[0]
-    plt.subplot(8,1,index)
-    plt.title(str(year), loc='right', y=0.5)
-    index += 1
-    plt.barh(agosto_2018.index, agosto_2018['val'])
-    #plt.tight_layout()
+
+    if year == 2011: 
+        df = pd.DataFrame(agosto_2018.values, index=agosto_2018.index, columns=[str(year)])
+    else: 
+        df[str(year)] = agosto_2018
     
+fig, ax = plt.subplots()
 
-#print(s / 8)
+im, cbar = H.heatmap(df, df.index, df.columns, ax=ax, cmap="YlGn", cbarlabel="Incidenti all'anno")
+texts = H.annotate_heatmap(im, valfmt="{x}")
+
+fig.tight_layout()
 plt.show()
-
