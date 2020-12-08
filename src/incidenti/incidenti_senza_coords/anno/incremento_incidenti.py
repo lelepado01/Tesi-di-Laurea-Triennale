5f05feaf939350f9ef5aa46107283f049cac1e3f
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-12-08 10:48:29 +0100

    modifiche a grafi

--- src/incidenti/incidenti_senza_coords/anno/incremento_incidenti.py
+++ src/incidenti/incidenti_senza_coords/anno/incremento_incidenti.py
@@ -1,13 +1,6 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import sys 
-sys.path.append("src/")
-
-import label_utils
-
-path = "dataset/incidenti/incidenti_2011.txt"
-data : pd.DataFrame = pd.read_csv(path, sep="\t")
 
 def count_people(row, campi) -> int: 
     count = 0
@@ -30,47 +23,7 @@ def get_people_in_vehicles(dataset : pd.DataFrame, fields):
 campi = ['veicolo__a___sesso_conducente', 'veicolo__a___et__passegger12']
 campi_2 = ['veicolo__a___sesso_conducente', 'veicolo__a___sesso_passegg37']
 
-#passeggeri_2010 = get_people_in_vehicles(pd.read_csv("dataset/incidenti/incidenti_2010.txt", sep="\t"), campi).value_counts()
-#print("Caricato 2010")
-#passeggeri_2011 = get_people_in_vehicles(pd.read_csv("dataset/incidenti/incidenti_2011.txt", sep="\t"), campi).value_counts()
-#print("Caricato 2011")
-#passeggeri_2012 = get_people_in_vehicles(pd.read_csv("dataset/incidenti/incidenti_2012.txt", sep="\t"), campi).value_counts()
-#print("Caricato 2012")
-#passeggeri_2013 = get_people_in_vehicles(pd.read_csv("dataset/incidenti/incidenti_2013.txt", sep="\t"), campi).value_counts()
-#print("Caricato 2013")
-#passeggeri_2014 = get_people_in_vehicles(pd.read_csv("dataset/incidenti/incidenti_2014.txt", sep="\t"), campi_2).value_counts()
-#print("Caricato 2014")
-#passeggeri_2015 = get_people_in_vehicles(pd.read_csv("dataset/incidenti/incidenti_2015.txt", sep="\t", encoding='latin1'), campi_2).value_counts()
-#print("Caricato 2015")
-#passeggeri_2016 = get_people_in_vehicles(pd.read_csv("dataset/incidenti/incidenti_2016.txt", sep="\t", encoding='latin1'), campi_2).value_counts()
-#print("Caricato 2016")
-
-#df = pd.DataFrame([
-#    #passeggeri_2010.iloc[0],
-#    #passeggeri_2011.iloc[0],
-#    #passeggeri_2012.iloc[0],
-#    #passeggeri_2013.iloc[0],
-#    passeggeri_2014.iloc[0],
-#    passeggeri_2015.iloc[0],
-#    passeggeri_2016.iloc[0]
-#], index=[
-#    #"passeggeri_2010",
-#    #"passeggeri_2011",
-#    #"passeggeri_2012",
-#    #"passeggeri_2013",
-#    "passeggeri_2014",
-#    "passeggeri_2015",
-#    "passeggeri_2016"
-#])
-#
-#print(df)
-#
-#df.plot()
-#plt.show()
-
-
 df = pd.DataFrame()
-
 for year in range(2010, 2019):
     l = 0
     if year < 2014:
@@ -92,14 +45,12 @@ i = [*range(2010, 2019)]
 i.remove(2017)
 df.index = i
 
-#print(df)
-
 df['numero'].plot(label='Incidenti con solo conducente', color='#4bccc9')
-df['sample_size'].plot(label='Taglia del campione', color='#4b8dcc')
+df['sample_size'].plot(label='Incidenti totali', color='#4b8dcc')
 plt.fill_between(df['sample_size'].index, df['sample_size'], color='#4b8dcc')
 plt.fill_between(df['numero'].index, df['numero'], color='#4bccc9')
 plt.xlabel("Anno")
 plt.ylabel("Numero di Incidenti")
-plt.legend(bbox_to_anchor=(1,1), loc="upper left")
+plt.legend(loc='lower right')
 plt.tight_layout()
 plt.show()
