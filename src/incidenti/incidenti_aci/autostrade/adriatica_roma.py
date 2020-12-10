ebd06e19adbcea5c3c4ba2830d279b72e536816f
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-12-10 08:48:52 +0100

    modifiche a grafo

--- src/incidenti/incidenti_aci/autostrade/adriatica_roma.py
+++ src/incidenti/incidenti_aci/autostrade/adriatica_roma.py
@@ -2,11 +2,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("dataset/incidenti/aci/autostrade/mesi_2014.csv")
-# data = pd.read_csv("dataset/incidenti/aci/autostrade/mesi_2016.csv")
-# data = pd.read_csv("dataset/incidenti/aci/autostrade/mesi_2018.csv")
-
-lombardia = data[data['REGIONE'] == 'Lombardia'][['PROVINCIA', 'TOTALE']]
+data = pd.read_csv("dataset/incidenti/aci/autostrade/mesi_2018.csv")
 
 def get_sum_fields(data : pd.DataFrame, select_field : str, field_to_sum : str) -> pd.Series: 
     res = {}
@@ -30,16 +26,16 @@ def sum_columns(data : pd.DataFrame, normalize = False) -> pd.Series:
 mesi = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'] 
 
 
+raccordo = data[data['CODICE'] == 'AA09001']
 adriatica = data[data['CODICE'] == 'SS01601']
-a1 = data[data['CODICE'] == 'AA00101']
-a1_milano = a1[a1['PROVINCIA'] == 'Milano'][mesi]
 
-a1 = sum_columns(a1[mesi], normalize=True)
-a1_milano = sum_columns(a1_milano[mesi], normalize=True)
-adriatica = sum_columns(adriatica[mesi], normalize=True)
+adriatica = sum_columns(adriatica[mesi])
+raccordo = sum_columns(raccordo[mesi])
 
-df = pd.DataFrame([a1, adriatica], ['A1 Milano', 'Adriatica']).transpose().plot.bar(width=0.9, color=['#a1cc61', '#61a1cc'])
-plt.ylabel("Percentuale di incidenti al mese (2014)")
+df = pd.DataFrame([adriatica, raccordo], ['Adriatica', 'Raccordo Anulare Roma']).transpose()
+
+df.plot.bar(width=0.9, color=['#a1cc61', '#61a1cc'])
+plt.ylabel("Numero di incidenti al mese (2018)")
 plt.tight_layout()
-plt.savefig("milano_adriatica_2014")
-# plt.show()
+# plt.savefig("milano_adriatica_2014")
+plt.show()
