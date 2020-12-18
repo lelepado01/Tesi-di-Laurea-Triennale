2b29abc1b0e9b6d314f0f2932f917bebc8057e6c
Gabriele Padovani <gabriele.padovani@gmail.com>
2020-12-18 17:27:07 +0100

    update di un grafo

--- src/incidenti/incidenti_senza_coords/mese_incidenti/mesi_estivi.py
+++ src/incidenti/incidenti_senza_coords/mese_incidenti/mesi_estivi.py
@@ -15,13 +15,13 @@ agosto_label = label_utils.join_labels(agosto, 'dataset/incidenti/Classificazion
 mesi = label_utils.join_labels(data['provincia'], 'dataset/incidenti/Classificazioni/provincia.csv').value_counts().head(10)
 #mesi = mesi / mesi.sum()
 
-agosto_ls = ['#8bc1c4']*12
-agosto_ls[:4] = ['#5bc1c6']*4
+agosto_ls = ['#6ac48f']*12
+agosto_ls[:4] = ['#1dad59']*4
 
-mesi_ls = ['#8fc1ab']*12
-mesi_ls[:2] = ['#5bc696']*2
-mesi_ls[7] = '#5bc696'
-mesi_ls[9] = '#5bc696'
+mesi_ls = ['#6a9ec4']*12
+mesi_ls[:2] = ['#1d71ad']*2
+mesi_ls[7] = '#1d71ad'
+mesi_ls[9] = '#1d71ad'
 
 plt.subplot(2,1,1)
 agosto_label.plot.bar(width=0.9, color=agosto_ls)
