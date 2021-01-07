d3c68dac20a9f1644b99397177ebb982fb1c0963
Gabriele Padovani <gabriele.padovani@gmail.com>
2021-01-07 10:50:33 +0100

    reso più preciso dataset dei tracciati autostradali vicini a milano

--- src/incidenti/incidenti_aci/autostrade/incidenti_line_chart.py
+++ src/incidenti/incidenti_aci/autostrade/incidenti_line_chart.py
@@ -41,7 +41,7 @@ for i in ore:
 
 strade = gp.GeoDataFrame(inc, geometry=strade['geometry'])
 
-ax = strade.plot(figsize=(11,9), column= 0, cmap='hot_r' ,legend=True, linewidth=1.5)
+ax = strade.plot(figsize=(11,9), column= 0, cmap='hot_r' ,legend=True, linewidth=1.8)
 plt.ylim((5.68 * pow(10, 6), 5.72 * pow(10,6)))
 plt.xlim((pow(10, 6), 1.04 * pow(10,6)))
 plt.xlabel("Incidenti nelle principali strade a Milano (2018)")
